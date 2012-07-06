1d6537a09de8eadc8edc80b0b2a78e9ca18cd1a4
Matthew Gidden <matthew.gidden@gmail.com>
2012-07-06 13:07:03 -0500

    updated growth work wrt recent major structural changes

---
+++ GrowthRegion.cpp
@@ -0,0 +1,178 @@
+// GrowthRegion.cpp
+// Implements the GrowthRegion class
+
+#include "GrowthRegion.h"
+
+#include "Model.h"
+#include "SupplyDemand.h"
+#include "BuildingManager.h"
+#include "SymbolicFunctions.h"
+
+#include <map>
+#include <vector>
+#include <string>
+#include <boost/shared_ptr.hpp>
+
+using namespace std;
+using namespace boost;
+
+
+/* --------------------
+ * GrowthRegion Class Methods
+ * --------------------
+ */
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+GrowthRegion::GrowthRegion() {
+  builders_ = map<Producer*,Model*>();
+  producers_ = map<Producer*,Model*>();
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+void GrowthRegion::init(xmlNodePtr cur) {
+  LOG(LEV_DEBUG2, "greg") << "A Growth Region is being initialized";
+  // xml inits
+  Model::init(cur); // name_ and model_impl_
+  RegionModel::initAllowedFacilities(cur); // allowedFacilities_
+
+  // get path to this model
+  xmlNodePtr model_cur = 
+    XMLinput->get_xpath_element(cur,"model/GrowthRegion");
+
+  // initialize supply demand manager
+  sdmanager_ = SupplyDemandManager();
+
+  xmlNodeSetPtr commodity_nodes = 
+    XMLinput->get_xpath_elements(model_cur,"gcommodity");\
+
+  for (int i=0;i<commodity_nodes->nodeNr;i++) {
+    // instantiate product
+    string name = 
+      (const char*)XMLinput->get_xpath_content(commodity_nodes[i], "name");
+    commodities_.push_back(name);
+    Commodity commodity(name);
+
+    // instantiate demand
+    string type = 
+      (const char*)XMLinput->get_xpath_content(commodity_nodes[i], "demand/type");
+    string params = 
+      (const char*)XMLinput->get_xpath_content(commodity_nodes[i], "demand/parameters");
+    FunctionPtr demand = SymbolicFunctions::getFunctionPtr(type,params);
+
+    // set up producers vector  
+    vector<Producer> producers;
+    xmlNodeSetPtr producer_nodes = 
+      XMLinput->get_xpath_elements(commodity_nodes[i],"metby");
+
+    for (int j=0;i<producer_nodes->nodeNr;i++){
+      string fac_name = 
+        (const char*)XMLinput->get_xpath_content(producer_nodes[j], "facility");
+      double capacity = 
+        (double)XMLinput->get_xpath_content(producer_nodes[j], "capacity");
+      Producer p(fac_name,commodity,capacity,1); // cost = 1
+      producers.push_back(p);
+    } // end producer nodes
+
+    manager.registerCommodity(commodity,demand,producers);
+  } // end commodity nodes
+
+  // instantiate building manager
+  buildmanager_ = BuildingManager(sdmanager_);
+  
+  // parent_ and tick listener, model 'born'
+  RegionModel::initSimInteraction(this); 
+  // children->setParent, requires init()
+  RegionModel::initChildren(cur); 
+
+  // populate producers_, builders_
+  populateProducerMaps();
+};
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+void GrowthRegion::copy(GrowthRegion* src) {
+  RegionModel::copy(src);
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+std::string GrowthRegion::str() {
+  std::string s = RegionModel::str();
+
+  // if ( builders_ == NULL || builders_->empty() ){
+  //   s += name() + " has no builders (currently)."; 
+  // } else {
+  //   s += name() + " has the following builders: " ; 
+  //   for(map<Model*, list<Model*>*>::iterator mit=builders_->begin();
+  //       mit != builders_->end(); mit++) {
+  //     s += " prototype=" + mit->first->name() + "("; 
+  //     for(list<Model*>::iterator inst = mit->second->begin();
+  //         inst != mit->second->end(); inst++) {
+  //       s += (*inst)->name() + ", "; 
+  //     }
+  //     s += "), ";
+  //   }
+  // }
+  return s;
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+void GrowthRegion::handleTick(int time) {
+  // After we finish building, call the normal handleTick for a region
+  RegionModel::handleTick(time);
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+void GrowthRegion::populateProducerMaps() {
+  map<string, pair<Model*,Model*> > producers_by_name;
+  
+  // if there are no children, yell
+  if ( children_.empty() ) {
+    stringstream err("");
+    err << "GrowthRegion " << this->name() << " cannot populate its list"
+        << " of builders because it has no children.";
+    throw CycOverrideException(err.str());
+  }
+
+  // for each child
+  for(vector<Model*>::iterator inst = children_.begin();
+      inst != children_.end(); inst++) {
+    // for each prototype of that child
+    for(PrototypeIterator 
+          fac = (dynamic_cast<InstModel*>(*inst))->beginPrototype();
+        fac != (dynamic_cast<InstModel*>(*inst))->endPrototype(); 
+        fac++) {
+      producers_by_name.insert( (*fac)->name(), pair<Model*,Model*>((*inst),(*fac)) );
+    }  // end prototypes
+  } // end children
+
+  // populate the maps
+  for (CommodityIterator ci = sdmanager_.begin(); ci != sdmanager_.end(); ci++) {
+    
+    
+  }
+  
+  
+  // if there are no builders, yell
+  if ( builders_->empty() ) {
+    stringstream err("");
+    err << "BuildRegion " << this->name() << " has finished populating"
+        << " its list of builders, but that list is empty.";
+    throw CycOverrideException(err.str());
+  }
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+void GrowthRegion::orderBuild(Model* builder, Model* buildee) {
+}
+/* -------------------- */
+
+
+/* --------------------
+ * Model Class Methods
+ * --------------------
+ */
+
+extern "C" Model* constructGrowthRegion() {
+    return new GrowthRegion();
+}
+
+/* -------------------- */
+
---
+++ GrowthRegion.h
@@ -0,0 +1,125 @@
+// GrowthRegion.h
+#ifndef GROWTHREGION_H
+#define GROWTHREGION_H
+
+#include "RegionModel.h"
+#include "Model.h"
+#include "SupplyDemand.h"
+#include "BuildingManager.h"
+
+#include <map>
+#include <string>
+
+/**
+   The GrowthRegion class inherits from the RegionModel class and is 
+   dynamically loaded by the Model class when requested. 
+    
+   This region determines if there is a need to meet a certain capacity 
+   (as defined via input) at each time step. If there is such a need, 
+   the region will determine how many of each facility type are required 
+   and then determine, facility by facility, which of its institutions 
+   are available to build each facility. 
+ */
+
+class GrowthRegion : public RegionModel  
+{
+/* --------------------
+ * all MODEL classes have these members
+ * --------------------
+ */
+ public:
+  /**
+     The default constructor for the GrowthRegion 
+   */
+  GrowthRegion();
+
+  /**
+     The default destructor for the GrowthRegion 
+   */
+  virtual ~GrowthRegion() {};
+
+  /**
+     Initalize the GrowthRegion from xml. Calls the init function. 
+     
+     @param cur the curren xml node pointer 
+   */
+  virtual void init(xmlNodePtr cur);
+
+  /**
+     initialize an object by copying another 
+   */
+  virtual void copy(GrowthRegion* src);
+
+  /**
+     This drills down the dependency tree to initialize all relevant 
+     parameters/containers. 
+     Note that this function must be defined only in the specific model 
+     in question and not in any inherited models preceding it. 
+      
+     @param src the pointer to the original (initialized ?) model to be 
+   */
+  virtual void copyFreshModel(Model* src) { 
+    copy(dynamic_cast<GrowthRegion*>(src)); 
+  }
+  
+  /**
+     print information about the region 
+   */
+  virtual std::string str();
+
+/* ------------------- */ 
+
+
+/* --------------------
+ * all COMMUNICATOR classes have these members
+ * --------------------
+ */
+ public:
+  /**
+     Each region is prompted to do its beginning-of-time-step stuff at 
+     the tick of the timer. The default behavior is to ignore the tick. 
+      
+     @param time is the time to perform the tick 
+   */
+  virtual void handleTick(int time);
+
+/* -------------------- */
+
+
+/* --------------------
+ * the GrowthRegion class has these members
+ * --------------------
+ */
+ protected:
+  /// a container of all commodities managed by region
+  std::vector<std::string> commodities_;
+
+  /// manager for supply and demand
+  SupplyDemandManager sdmanager_;
+
+  /// manager for building things
+  BuildingManager buildmanager_;
+
+  /// a map for the institutions that can build a prototype
+  std::map<Producer*,Model*> builders_;
+
+  /// a map for the prototypes that correspond to supplydemand's producer
+  std::map<Producer*,Model*> producers_;
+
+  /**
+     populates builders_ and producers_ once all initialization is complete
+   */
+  void populateProducerMaps();
+
+  /**
+     orders builder to build buildee
+     @param builder the model that can build buildee
+     @param buildee the model to be built
+   */
+  void orderBuild(Model* builder, Model* buildee);
+
+/* ------------------- */ 
+
+};
+
+#endif
