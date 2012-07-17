b332a846330bad9f3f67c6ddbc2158e2f4aca279
Katy Huff <katyhuff@gmail.com>
2012-07-17 15:24:24 -0500

    A much better solution for rng configuration. Inspired by @rwcarlsen's bug report, #48 .

--- CMake/CycamoreModuleMacros.cmake
+++ CMake/CycamoreModuleMacros.cmake
@@ -8,7 +8,6 @@ macro(cyclus_init  _path _name)
   # Link the libraries to libcycluscore 
   TARGET_LINK_LIBRARIES(${_name} dl ${LIBS})
   SET(CYCAMORE_LIBRARIES ${CYCAMORE_LIBRARIES} ${_name} )
-
   
   CONFIGURE_FILE(
     ${CYCAMORE_SOURCE_DIR}${_path}/${_name}.rng
@@ -20,10 +19,6 @@ macro(cyclus_init  _path _name)
     LIBRARY DESTINATION cyclus/lib${_path}
     COMPONENT ${_path}
     )
-  
-  SET(RNG_INCLUDES ${RNG_INCLUDES}
-    "<include href='../lib${_path}/${_name}.rng'/>"
-    PARENT_SCOPE)
 
   install(FILES "${_name}.rng"
     DESTINATION cyclus/lib${_path}
@@ -40,69 +35,56 @@ macro(cyclus_init_model _type _name)
     ${CMAKE_CURRENT_SOURCE_DIR}/${_name}.cpp 
     ${CMAKE_CURRENT_SOURCE_DIR}/${_name}Tests.cpp 
     PARENT_SCOPE)
-
-  SET(${_type}_REFS ${${_type}_REFS}
-    "<ref name='${_name}'/>"
-    PARENT_SCOPE)
 endmacro()
 
-
-macro(update_refs _type)
-  SET(${_type}_REFS ";${_type}_REFS;")
-  STRING(REPLACE ";" "@" ${_type}_REFS "${${_type}_REFS}")
-  SET(${_type}_EXTEND "@${_type}_REFS@")
+macro(find_modules _type)
+  SET(TYPE_PATH "${CYCLUS_ROOT_DIR}/lib/Models/${_type}/*/")
+  FILE(GLOB all_valid_subdirs ${TYPE_PATH} "*/*.rng")
+  FOREACH(dir ${all_valid_subdirs})
+    STRING(REPLACE "${CYCLUS_ROOT_DIR}/lib/Models/${_type}/" "" model_name "${dir}" )
+    SET(${_type}_REFS ${${_type}_REFS} "<ref name='${model_name}'/>")
+    SET(RNG_INCLUDES ${RNG_INCLUDES} "<include href='../lib/Models/${_type}/${model_name}/${model_name}.rng'/>")
+  ENDFOREACH(dir) 
 endmacro()
 
-
-macro(update_includes)
-  SET(RNG_INCLUDES ";RNG_INCLUDES;")
-  STRING(REPLACE ";" "@" RNG_INCLUDES "${RNG_INCLUDES}")
-  SET(EXTEND "@RNG_INCLUDES@")
+macro(find_all_modules)
+  find_modules(Facility)
+  find_modules(Inst)
+  find_modules(Region)
+  find_modules(Market)
+  find_modules(Converter)
+  find_modules(Stub)
+  find_modules(StubComm)
 endmacro()
 
-macro(extend_includes)
-  SET(RNG_INCLUDES "@RNG_INCLUDES@@EXTEND@")
+macro(clean_includes)
+  STRING(REPLACE ";" "\n\ \ " RNG_INCLUDES "${RNG_INCLUDES}")
 endmacro()
 
-macro(extend_refs _type)
-  SET(${_type}_REFS "@${_type}_REFS@@${_type}_EXTEND@")
+macro(clean_refs _type)
+  STRING(REPLACE ";" "\n\ \ \ \ \ \ \ \ " ${_type}_REFS "${${_type}_REFS}")
 endmacro()
 
-macro(dont_extend_refs _type)
-  SET(${_type}_REFS "")
-  extend_refs(${_type})
+macro(clean_all_refs)
+  clean_refs(Facility)
+  MESSAGE(STATUS "Facility_REFS ${Facility_REFS}") 
+  clean_refs(Inst)
+  clean_refs(Region)
+  clean_refs(Market)
+  clean_refs(Converter)
+  clean_refs(Stub)
+  clean_refs(StubComm)
 endmacro()
 
-macro(extend _type)
-  IF(${_type}_REFS MATCHES "ref")
-    extend_refs(${_type})
-  ELSE(${_type}_REFS MATCHES "ref")
-    dont_extend_refs(${_type})
-  ENDIF(${_type}_REFS MATCHES "ref")
-endmacro()
-
-macro(install_rng_in)
-  extend_includes()
-  extend(Facility)
-  extend(Inst)
-  extend(Region)
-  extend(Market)
-  extend(Converter)
+macro(install_rng)
+  find_all_modules()
+  clean_includes()
+  clean_all_refs()
   CONFIGURE_FILE(${CYCLUS_CORE_SHARE_DIR}/cyclus.rng.in
-    ${PROJECT_BINARY_DIR}/share/cyclus.rng.tmp @ONLY)
-  update_includes()
-  update_refs(Facility)
-  update_refs(Inst)
-  update_refs(Region)
-  update_refs(Market)
-  update_refs(Converter)
-  CONFIGURE_FILE(${PROJECT_BINARY_DIR}/share/cyclus.rng.tmp
-    ${PROJECT_BINARY_DIR}/share/cyclus.rng.in @ONLY)
+    ${PROJECT_BINARY_DIR}/share/cyclus.rng @ONLY)
   INSTALL(FILES
-    ${PROJECT_BINARY_DIR}/share/cyclus.rng.in
+    ${PROJECT_BINARY_DIR}/share/cyclus.rng
     DESTINATION ${CYCLUS_CORE_SHARE_DIR}
     COMPONENT data
   )
 endmacro()
-
-
