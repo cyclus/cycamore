46999f20ebd900899d982ad128f88b969b614307
Matthew Gidden <matthew.gidden@gmail.com>
2013-09-05 16:53:20 -0500

    added enrichment facility to context usage

--- src/Models/Facility/EnrichmentFacility/enrichment_facility_tests.cc
+++ src/Models/Facility/EnrichmentFacility/enrichment_facility_tests.cc
@@ -8,13 +8,13 @@
 
 #include "commodity.h"
 #include "xml_query_engine.h"
-#include "recipe_library.h"
 
 #include <sstream>
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void EnrichmentFacilityTest::SetUp() {
-  src_facility = new cycamore::EnrichmentFacility();
+  cyclus::Context* ctx = tc_.get();
+  src_facility = new cycamore::EnrichmentFacility(ctx);
 
   InitParameters();
   InitFacility();
@@ -29,13 +29,15 @@ void EnrichmentFacilityTest::TearDown() {
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void EnrichmentFacilityTest::InitParameters() {
+  cyclus::Context* ctx = tc_.get();
+  
   in_commod = "incommod";
-  in_commod_market = new TestMarket();
+  in_commod_market = new TestMarket(ctx);
   in_commod_market->SetCommodity(in_commod);
   cyclus::MarketModel::RegisterMarket(in_commod_market);
 
   out_commod = "outcommod";
-  out_commod_market = new TestMarket();
+  out_commod_market = new TestMarket(ctx);
   out_commod_market->SetCommodity(out_commod);
   cyclus::MarketModel::RegisterMarket(out_commod_market);
 
@@ -46,7 +48,7 @@ void EnrichmentFacilityTest::InitParameters() {
   v[92235] = feed_assay;
   v[92238] = 1 - feed_assay;
   recipe = cyclus::Composition::CreateFromAtom(v);
-  cyclus::RL->AddRecipe(in_recipe, recipe);
+  ctx->RegisterRecipe(in_recipe, recipe);
 
   tails_assay = 0.002;
   inv_size = 5;
@@ -89,7 +91,10 @@ TEST_F(EnrichmentFacilityTest, init) {
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 TEST_F(EnrichmentFacilityTest, clone) {
-  cycamore::EnrichmentFacility* cloned_fac = new cycamore::EnrichmentFacility();
+  cyclus::Context* ctx = tc_.get();
+  
+  cycamore::EnrichmentFacility* cloned_fac =
+      new cycamore::EnrichmentFacility(ctx);
   cloned_fac->CloneModuleMembersFrom(src_facility);
 
   EXPECT_EQ(in_recipe, cloned_fac->in_recipe());
@@ -103,14 +108,16 @@ TEST_F(EnrichmentFacilityTest, clone) {
   delete cloned_fac;
 }
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-cyclus::Model* EnrichmentFacilityModelConstructor() {
-  return dynamic_cast<cyclus::Model*>(new cycamore::EnrichmentFacility());
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+cyclus::Model* EnrichmentFacilityModelConstructor(cyclus::Context* ctx) {
+  using cycamore::EnrichmentFacility;
+  return dynamic_cast<cyclus::Model*>(new EnrichmentFacility(ctx));
 }
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-cyclus::FacilityModel* EnrichmentFacilityConstructor() {
-  return dynamic_cast<cyclus::FacilityModel*>(new cycamore::EnrichmentFacility());
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+cyclus::FacilityModel* EnrichmentFacilityConstructor(cyclus::Context* ctx) {
+  using cycamore::EnrichmentFacility;
+  return dynamic_cast<cyclus::FacilityModel*>(new EnrichmentFacility(ctx));
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
