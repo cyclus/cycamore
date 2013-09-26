2fb6f1f953b0f0d2db4ae408a5041608edb3b67e
Olzhas <olzhasrakhimov7@gmail.com>
2013-09-26 17:00:15 -0500

    Successfull call of cyclus from python.

--- tests/system_call.py
+++ tests/system_call.py
@@ -4,3 +4,20 @@ import subprocess
 def test_cyclus_call():
     subprocess.call(['cyclus', './null_sink.xml'])
     subprocess.call(['cyclus', './source_to_sink.xml'])
+   
+def test_ls():
+    subprocess.call(['ls','-alh'], shell=True,stdout=False)
+        
+def test_print():
+    subprocess.call(['echo','On','Wisconsin'],shell=True, stdout=False)
+
+def test_cyclus_unit_tests():
+    subprocess.call(['/home/olzhas/cyclus/install/bin/CyclusUnitTestDriver'])
+    subprocess.call(['/home/olzhas/cyclus/install/bin/CycamoreUnitTestDriver'])
+    subprocess.call(['python','home/olzhas/cyclus/install/bin/run_inputs.py'])
+
+def test_cyclus_call():
+    subprocess.call(['/home/olzhas/cyclus/install/bin/cyclus',
+        './null_sink.xml'])
+    subprocess.call(['/home/olzhas/cyclus/install/bin/cyclus',
+        './source_to_sink.xml'])
