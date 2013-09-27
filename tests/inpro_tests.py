9b6354b5749de82c73680408508f3b2fc4a965f5
Olzhas <olzhasrakhimov7@gmail.com>
2013-09-27 16:16:02 -0500

    Initial setup for inpro input tests.

--- tests/inpro_tests.py
+++ tests/inpro_tests.py
@@ -3,6 +3,6 @@ import subprocess
 
 def test_cyclus_call():
     subprocess.call(['cyclus','-o','output.h5','--input-file',
-    './null_sink.xml'])
+    '$CYCAMORE_PATH/input/inpro/inpro_low.xml'])
     subprocess.call(['cyclus', '-o','output.h5','--input-file',
-    './source_to_sink.xml'])
+    '/home/olzhas/cyclus/cycamore/input/inpro/inpro_high.xml'])
