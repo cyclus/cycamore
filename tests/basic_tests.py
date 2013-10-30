124a459e650454aa56ef7a35b59d5ce077b0ba91
Olzhas <olzhasrakhimov7@gmail.com>
2013-10-30 08:58:02 -0500

    Unused null and simple source-to-sink call of cyclus are deleted.

--- tests/basic_tests.py
+++
@@ -1,8 +0,0 @@
-
-import subprocess
-
-def test_cyclus_call():
-    subprocess.call(['cyclus','-o','output.h5','--input-file',
-    './null_sink.xml'])
-    subprocess.call(['cyclus', '-o','output.h5','--input-file',
-    './source_to_sink.xml'])
