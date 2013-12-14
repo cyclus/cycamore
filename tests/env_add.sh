56a7eff6df61087b1fef9ef43198d213c924bc66
Olzhas <olzhasrakhimov7@gmail.com>
2013-12-14 14:32:50 -0600

    Correction #! /bin/bash in env_add.sh

--- tests/env_add.sh
+++ tests/env_add.sh
@@ -1,7 +1,7 @@
-#! /usr/bin/bash
+#! /bin/bash
 
-#Adds PyTables 3 built in my directory on CAE
-#Updates PYTHONPATH, LD_LIB_PATH variables
+# Adds PyTables 3 built in my directory on CAE
+# Updates PYTHONPATH, LD_LIB_PATH variables
 
 export PYTHONPATH=$PYTHONPATH:$CNERG_ROOT/users/rakhimov/install/lib/python2.7/site-packages
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CNERG_ROOT/users/rakhimov/hdf5-1.8.11/hdf5/lib
