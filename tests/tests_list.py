e97649a29a10ecc06eaa33b0898b2c22368e7102
Olzhas <olzhasrakhimov7@gmail.com>
2014-01-08 13:18:17 -0600

    Add python file with a list of simulation files.

---
+++ tests/tests_list.py
@@ -0,0 +1,4 @@
+
+#List of input files and reference databases
+sim_files = [("./inputs/physor/1_Enrichment_2_Reactor.xml", "./benchmarks/physor_1_Enrichment_2_Reactor.h5"),
+            ("./inputs/physor/2_Sources_3_Reactors.xml", "./benchmarks/physor_2_Sources_3_Reactors.h5")]
