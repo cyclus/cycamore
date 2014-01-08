3610d122916b46b3328b5770d45d85f0750b9a0d
Olzhas <olzhasrakhimov7@gmail.com>
2014-01-08 14:03:00 -0600

    Add cyclus_tools.py with a function to call cyclus

---
+++ tests/cyclus_tools.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+from tools import check_cmd
+
+def run_cyclus(cyclus, cwd, sim_files):
+    """Runs cyclus with various inputs and creates output databases
+       """
+
+    for sim_input, sim_output in zip(sim_files):
+        holdsrtn = [1]  # needed because nose does not send() to test generator
+        cmd = [cyclus, "-o", sim_output, "--input-file", sim_input]
+        check_cmd(cmd, cwd, holdsrtn)
+        rtn = holdsrtn[0]
+        if rtn != 0:
+            return  # don"t execute further commands
