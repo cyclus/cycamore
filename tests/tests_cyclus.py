9dc4f27fd9288b5bee6bf4ef1dbe16484783882e
Olzhas <olzhasrakhimov7@gmail.com>
2014-01-08 16:42:33 -0600

    Add main regression test file for cyclus

---
+++ tests/tests_cyclus.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+import os
+
+from tests_list import sim_files
+from cyclus_tools import run_cyclus, db_comparator
+
+"""Tests"""
+def test_cyclus():
+    """Test for all inputs in sim_files. Checks if reference and current cyclus 
+    output is the same.
+
+    WARNING: the tests require cyclus executable to be included in PATH
+    """
+    cwd = os.getcwd()
+    
+    for sim_input,bench_db in sim_files:
+
+        temp_output = [(sim_input, "./output_temp.h5")]
+        yield run_cyclus("cyclus", cwd, temp_output)
+
+        yield db_comparator(args, bench_db, "./output_temp.h5")
+
+        os.remove("./output_temp.h5")
