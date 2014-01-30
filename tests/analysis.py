4f4938b042224894860c4b77a3a29a60b538d338
gidden <matthew.gidden@gmail.com>
2014-01-30 16:38:56 -0600

    changed analysis module to use multiprocess

--- tests/analysis.py
+++ tests/analysis.py
@@ -2,10 +2,43 @@ from __future__ import print_function
 import test_regression as tst
 
 import subprocess
+from multiprocessing import Pool, Manager, cpu_count
 from collections import defaultdict
 
 diff_tbl = """table is different"""
 diff_col = """Column"""
+
+def add_tbl(dic, tbl):
+    """adds a table in a defaultdict-like manner
+    """
+    if tbl in dic:
+        dic[tbl] += 1
+    else: 
+        dic[tbl] = 1
+
+def collect(args):
+    """collects information on a given nosetest run
+    """
+    tbl_freq, col_freq = args
+    rtn = subprocess.Popen(["nosetests", "-s"], stdout=subprocess.PIPE, 
+                           stderr=subprocess.PIPE)
+    out, err = rtn.communicate()
+    for line in out.split("\n"):
+        line = line.strip()
+        if diff_tbl in line.strip():
+            tbl_name = line.split()[0]
+            add_tbl(tbl_freq, tbl_name)
+        if diff_col in line.strip():
+            col_name = line.split()[1]
+            col_freq.append((tbl_name, col_name))
+
+def proxy_lst_to_dict(lst):
+    """converts the col_freq list into a dictionary for easier processing
+    """
+    col_freq = defaultdict(lambda: defaultdict(int))
+    for tbl, col in lst:
+        col_freq[tbl][col] += 1
+    return col_freq
 
 def determ_analysis(niter = 1000, fname = "report"):
     """
@@ -15,28 +48,30 @@ def determ_analysis(niter = 1000, fname = "report"):
     Note that the tests that nosetest will call must be configured to test the
     supposed deterministic cases.
     """
-    tbl_freq = defaultdict(int)
-    col_freq = defaultdict(lambda: defaultdict(int))
-    
+    m = Manager()
+
+    tbl_freq = m.dict()
+    col_freq = m.list()
+
     # collect
-    for i in range(niter):
-        rtn = subprocess.Popen(["nosetests", "-s"], stdout=subprocess.PIPE, 
-                               stderr=subprocess.PIPE)
-        out, err = rtn.communicate()
-        for line in out.split("\n"):
-            line = line.strip()
-            if diff_tbl in line.strip():
-                tbl_name = line.split()[0]
-                tbl_freq[tbl_name] += 1 
-            if diff_col in line.strip():
-                col_freq[tbl_name][line.split()[1]] += 1
+    nproc = cpu_count()
+    count = nproc if nproc == 1 else nproc - 1
+    pool  = Pool(count)
+    args = ((tbl_freq, col_freq) for i in range(niter))
+    pool.map_async(collect, args)
+    pool.close()
+    pool.join()
+
+    # convert from proxy
+    col_freq = proxy_lst_to_dict(col_freq)
+    tbl_freq = {item[0]: item[1] for item in tbl_freq.items()}
 
     # normalize
     for tbl, dic in col_freq.iteritems():
         for col, freq in dic.iteritems():
-            dic[col] = "{0:.2f}".format(float(freq) / tbl_freq[tbl])
+            dic[col] = "{0:.2f}".format(float(freq) / tbl_freq[tbl])    
     for k, v in tbl_freq.iteritems():
-        tbl_freq[k] = "{0:.2f}".format(float(v) / n)
+        tbl_freq[k] = "{0:.2f}".format(float(v) / niter)
     
     # report
     lines = []
