a0f01cc65d3d9891e4724f74b2e7193aad2f9ece
Matthew Gidden <matthew.gidden@gmail.com>
2014-02-03 14:59:04 -0600

    pep8 and incorporated @scopatz's suggestions for line reduction

--- tests/analysis.py
+++ tests/analysis.py
@@ -8,28 +8,22 @@ from collections import defaultdict
 diff_tbl = """table is different"""
 diff_col = """Column"""
 
-def add_tbl(dic, tbl):
-    """adds a table in a defaultdict-like manner
-    """
-    if tbl in dic:
-        dic[tbl] += 1
-    else: 
-        dic[tbl] = 1
-
 def collect(args):
     """collects information on a given nosetest run
     """
     tbl_freq, col_freq = args
+
     rtn = subprocess.Popen(["nosetests", "-s"], stdout=subprocess.PIPE, 
                            stderr=subprocess.PIPE)
     out, err = rtn.communicate()
+
     for line in out.split("\n"):
         line = line.strip()
+        tbl_name = line.split()[0]
+        col_name = line.split()[1]
         if diff_tbl in line.strip():
-            tbl_name = line.split()[0]
-            add_tbl(tbl_freq, tbl_name)
+            tbl_freq[tbl_name] = tbl_freq[tbl_name] + 1 if tbl_name in tbl_freq else 1
         if diff_col in line.strip():
-            col_name = line.split()[1]
             col_freq.append((tbl_name, col_name))
 
 def proxy_lst_to_dict(lst):
@@ -40,7 +34,7 @@ def proxy_lst_to_dict(lst):
         col_freq[tbl][col] += 1
     return col_freq
 
-def determ_analysis(niter = 1000, fname = "report"):
+def determ_analysis(niter=1000, fname="report"):
     """
     Calls nosetests for a number of iterations and reports findings of
     nondeterminism to a file.
@@ -83,6 +77,4 @@ def determ_analysis(niter = 1000, fname = "report"):
         f.writelines(lines)
 
 if __name__ == "__main__":
-    niter = 100
-    fname = "report"
-    determ_analysis(niter, fname)
+    determ_analysis(niter=100)
