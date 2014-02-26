791a6c1060660f53f15dcc0aae2a2d7b419fc872
Olzhas <olzhasrakhimov7@gmail.com>
2014-02-26 16:28:05 -0600

    null_inst* : style corrections.

--- src/Models/NullInst/null_inst_tests.cc
+++ src/Models/NullInst/null_inst_tests.cc
@@ -7,7 +7,7 @@
 #include "inst_model_tests.h"
 #include "model_tests.h"
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 class NullInstTest : public ::testing::Test {
  protected:
   virtual void SetUp() {}
@@ -15,18 +15,19 @@ class NullInstTest : public ::testing::Test {
   virtual void TearDown() {}
 };
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 cyclus::Model* NullInstModelConstructor(cyclus::Context* ctx) {
   return dynamic_cast<cyclus::Model*>(new cycamore::NullInst(ctx));
 }
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 cyclus::InstModel* NullInstConstructor(cyclus::Context* ctx) {
   return dynamic_cast<cyclus::InstModel*>(new cycamore::NullInst(ctx));
 }
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-INSTANTIATE_TEST_CASE_P(NullInst, InstModelTests, Values(&NullInstConstructor));
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+INSTANTIATE_TEST_CASE_P(NullInst, InstModelTests,
+                        Values(&NullInstConstructor));
 INSTANTIATE_TEST_CASE_P(NullInst, ModelTests,
                         Values(&NullInstModelConstructor));
 
