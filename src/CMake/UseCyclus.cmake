817f224fe77c49eada0456d6468a52aaafde4c60
gidden <matthew.gidden@gmail.com>
2014-03-21 13:54:15 -0500

    test cycluscore->libs

--- src/CMake/UseCyclus.cmake
+++ src/CMake/UseCyclus.cmake
@@ -183,8 +183,8 @@ MACRO(USE_CYCLUS lib_root src_root)
       )
 
     ADD_LIBRARY(${lib_root}Tests ${${lib_root}_TEST_CC})
-    TARGET_LINK_LIBRARIES(${lib_root}Tests dl cycluscore ${CYCLUS_GTEST_LIBRARIES})
-    SET_TARGET_PROPERTIES(${lib_root}Tests PROPERTIES LINKER_LANGUAGE C)
+    TARGET_LINK_LIBRARIES(${lib_root}Tests dl ${LIBS} ${CYCLUS_GTEST_LIBRARIES})
+    SET_TARGET_PROPERTIES(${lib_root}Tests PROPERTIES LINKER_LANGUAGE CXX)
     SET(
       "${lib_root}_TEST_LIB"
       "${lib_root}Tests"
