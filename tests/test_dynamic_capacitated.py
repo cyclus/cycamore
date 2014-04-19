8d549ff0106da439ed85016d4a68873a58b4c476
rakhimov <olzhasrakhimov7@gmail.com>
2014-04-18 21:47:54 -0500

    Test dynamic capacitated is implemented.

--- tests/test_dynamic_capacitated.py
+++ tests/test_dynamic_capacitated.py
@@ -12,6 +12,18 @@ from helper import table_exist, find_ids, exit_times
 def test_dynamic_capacitated():
     """Tests dynamic capacity restrains involving changes in the number of
     source and sink facilities.
+
+    A source facility is expected to offer a commodity of amount 1,
+    and a sink faciliity is expected to request for a commodity of amount 1.
+    Therefore, number of facilities correspond to the amounts of offers
+    and requests.
+
+    At time step 1, 3 source facilities and 2 sink facilities are deployed, and
+    at time step 2, additional 2 sink facilities are deployed.
+    According to this deployment schedule, at time step 1, only 2 transactions
+    are expected, the number of sink facilities being the constraint; whereas,
+    at time step 2, only 3 transactions are expected, the number of source
+    facilities being the constraint.
     """
     # Cyclus simulation input for dynamic capacitated
     sim_inputs = ["../input/dynamic_capacitated/dynamic_capacitated.xml"]
@@ -48,6 +60,7 @@ def test_dynamic_capacitated():
         # Find agent ids of source and sink facilities
         agent_ids = agent_entry["AgentId"]
         agent_impl = agent_entry["Implementation"]
+        depl_time = agent_entry["EnterTime"]
 
         source_id = find_ids("Source", agent_impl, agent_ids)
         sink_id = find_ids("Sink", agent_impl, agent_ids)
@@ -56,13 +69,52 @@ def test_dynamic_capacitated():
         yield assert_equal, len(source_id), 3
         yield assert_equal, len(sink_id), 4
 
-        # Check if transactions are only between source and sink
+        # Test that source facilities are all deployed at time step 1
+        for s in source_id:
+            yield assert_equal, depl_time[np.where(agent_ids == s)], 1
+        # Test that first 2 sink facilities are deployed at time step 1
+        for i in [0, 1]:
+            yield assert_equal,\
+                    depl_time[np.where(agent_ids == sink_id[i])], 1
+        for i in [2, 3]:
+            yield assert_equal,\
+                    depl_time[np.where(agent_ids == sink_id[i])], 2
+
+        # Check transactions
         sender_ids = transactions["SenderId"]
         receiver_ids = transactions["ReceiverId"]
         trans_time = transactions["Time"]
+        trans_resource = transactions["ResourceId"]
         # Track transacted resources
         resource_ids = resources["ResourceId"]
         quantities = resources["Quantity"]
+
+        # Check that transactions are between sources and sinks only
+        for s in sender_ids:
+            yield assert_equal, len(np.where(source_id == s)), 1
+
+        for r in receiver_ids:
+            yield assert_equal, len(np.where(sink_id == s)), 1
+
+        # Check that at time step 1, there are 2 transactions
+        yield assert_equal, len(trans_time), 5
+        yield assert_equal, len(np.where(trans_time == 1)[0]), 2
+        # Check that at time step 2, there are 3 transactions
+        yield assert_equal, len(np.where(trans_time == 2)[0]), 3
+
+        # Check that at time step 1, there are 2 transactions with total
+        # amount of 2
+        quantity = 0
+        for t in np.where(trans_time == 1)[0]:
+            quantity += quantities[np.where(resource_ids == trans_resource[t])]
+        yield assert_equal, quantity, 2
+
+        # Check that at time step 2, there are 3 transactions with total
+        # amount of 3
+        quantity = 0
+        for t in np.where(trans_time == 2)[0]:
+            quantity += quantities[np.where(resource_ids == trans_resource[t])]
+        yield assert_equal, quantity, 3
 
         output.close()
         os.remove("./output_temp.h5")
