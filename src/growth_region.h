c52338f2de83840cff26930bbc0ae6afa1ca4bd5
Anthony Scopatz <scopatz@gmail.com>
2014-05-22 17:24:40 -0500

    minor updates

--- src/growth_region.h
+++ src/growth_region.h
@@ -1,14 +1,11 @@
 // growth_region.h
-#ifndef GROWTHREGION_H
-#define GROWTHREGION_H
-
-#include "region.h"
-#include "commodity.h"
-#include "supply_demand_manager.h"
-#include "building_manager.h"
-//#include "InfileTree.h"//
+#ifndef CYCAMORE_SRC_GROWTH_REGION_H_
+#define CYCAMORE_SRC_GROWTH_REGION_H_
 
 #include <set>
+
+#include "cyclus.h"
+
 
 // forward declarations
 namespace cycamore {
@@ -39,61 +36,31 @@ struct DemandInfo {
 class GrowthRegion : public cyclus::Region {
   friend class GrowthRegionTests;
  public:
-  /* --- Module Members --- */
-  /**
-     The default constructor for the GrowthRegion
-   */
+  /// The default constructor for the GrowthRegion
   GrowthRegion(cyclus::Context* ctx);
 
-  /**
-     The default destructor for the GrowthRegion
-   */
+  /// The default destructor for the GrowthRegion
   virtual ~GrowthRegion();
 
-  virtual std::string schema();
+  #pragma cyclus decl
 
-  /* --- */
-
-  /* --- Region Members --- */
-  /**
-     Initialize members related to derived module class
-     @param qe a pointer to a cyclus::InfileTree object containing initialization data
-   */
-  virtual void InitFrom(cyclus::InfileTree* qe);
-
-  /**
-     add a demand for a commodity on which this region request that
-     facilities be built
-   */
+  /// add a demand for a commodity on which this region request that
+  /// facilities be built
   void AddCommodityDemand(cyclus::Commodity commod);
 
-  /**
-     perform module-specific tasks when entering the simulation
-   */
+  /// perform module-specific tasks when entering the simulation
   virtual void Build(cyclus::Agent* parent);
 
-  /**
-     On each tick, the GrowthRegion queries its supply demand manager
-     to determine if there exists some demand. If demand for a
-     commodity exists, then the correct build order for that demand
-     is constructed and executed.
-     @param time is the time to perform the tick
-   */
+  /// On each tick, the GrowthRegion queries its supply demand manager
+  /// to determine if there exists some demand. If demand for a
+  /// commodity exists, then the correct build order for that demand
+  /// is constructed and executed.
+  /// @param time is the time to perform the tick
   virtual void Tick(int time);
-  /* --- */
-
-  virtual cyclus::Agent* Clone() {
-    GrowthRegion* m = new GrowthRegion(context());
-    m->InitFrom(this);
-    return m;
-  }
-
-  void InitFrom(GrowthRegion* m);
 
   virtual void BuildNotify(Agent* m);
 
  protected:
-  /* --- GrowthRegion Members --- */
   /// a container of all commodities managed by region
   std::set<cyclus::Commodity, cyclus::CommodityCompare> commodities_;
 
@@ -105,33 +72,25 @@ class GrowthRegion : public cyclus::Region {
   /// manager for Supply and demand
   cyclus::SupplyDemandManager sdmanager_;
 
-  /**
-     register a child as a commodity producer manager if it is one
-     @param agent the child to register
-   */
+  /// register a child as a commodity producer manager if it is one
+  /// @param agent the child to register
   void RegisterCommodityProducerManager(cyclus::Agent* agent);
 
-  /**
-     register a child as a builder if it is one
-     @param agent the child to register
-   */
+  /// register a child as a builder if it is one
+  /// @param agent the child to register
   void RegisterBuilder(cyclus::Agent* agent);
 
-  /**
-     orders builds given a commodity and an unmet demand for production
-     capacity of that commodity
-     @param commodity the commodity being demanded
-     @param unmetdemand the unmet demand
-   */
+  /// orders builds given a commodity and an unmet demand for production
+  /// capacity of that commodity
+  /// @param commodity the commodity being demanded
+  /// @param unmetdemand the unmet demand
   void OrderBuilds(cyclus::Commodity& commodity, double unmetdemand);
 
-  /**
-     orders builder to build a prototype
-     @param builder the agent that can build buildee
-     @param prototype the agent to be built
-   */
+  /// orders builder to build a prototype
+  /// @param builder the agent that can build buildee
+  /// @param prototype the agent to be built
   void OrderBuild(cyclus::Agent* builder, cyclus::Agent* prototype);
-  /* --- */
 };
 } // namespace cycamore
-#endif
+
+#endif  // CYCAMORE_SRC_GROWTH_REGION_H_
