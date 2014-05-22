79e70b41c96b5f8de72e047ba315aaa13f2b86b4
Anthony Scopatz <scopatz@gmail.com>
2014-05-22 18:28:07 -0500

    something was done

--- src/manager_inst.h
+++ src/manager_inst.h
@@ -1,75 +1,41 @@
 // manager_inst.h
-#ifndef _MANAGERINST_H
-#define _MANAGERINST_H
+#ifndef CYCAMORE_SRC_MANAGER_INST_H_
+#define CYCAMORE_SRC_MANAGER_INST_H_
 
-#include "institution.h"
-#include "builder.h"
-#include "commodity_producer_manager.h"
-#include "commodity_producer.h"
+#include "cyclus.h"
 
 namespace cycamore {
 
-/**
-   @class ManagerInst
-
-   @section introduction Introduction
-
-   @section detailedBehavior Detailed Behavior
-*/
+/// @class ManagerInst
+/// @section introduction Introduction
+/// @section detailedBehavior Detailed Behavior
 class ManagerInst : public cyclus::Institution,
-  public cyclus::CommodityProducerManager,
-  public cyclus::Builder {
+  public cyclus::toolkit::CommodityProducerManager,
+  public cyclus::toolkit::Builder {
  public:
-  /**
-     Default constructor
-   */
+  /// Default constructor
   ManagerInst(cyclus::Context* ctx);
 
-  /**
-     Default destructor
-   */
+  /// Default destructor
   virtual ~ManagerInst();
 
-  virtual cyclus::Agent* Clone() {
-    ManagerInst* m = new ManagerInst(context());
-    m->InitFrom(this);
-    return m;
-  }
+  #pragma cyclus
 
-  void InitFrom(ManagerInst* m) {
-    Institution::InitFrom(m);
-    commod_producers_ = m->commod_producers_;
-  }
-
-  virtual void InitFrom(cyclus::InfileTree* qe);
-
-  /**
-     perform any actions required after prototype has been added to
-     the list of available prototypes
-     @param prototype the prototype to register
-   */
-  void RegisterAvailablePrototype(std::string prototype);
-
-  /**
-     perform any registration functionality after a clone has been
-     built
-     @param clone the built (cloned) prototype
-   */
+  /// perform any registration functionality after a clone has been
+  /// built
+  /// @param clone the built (cloned) prototype
   virtual void BuildNotify(cyclus::Agent* clone);
 
-  /**
-     perform any registration functionality before a clone is
-     decommissioned(deleted)
-     @param clone the to-be-decommissioned prototype
-   */
+  /// perform any registration functionality before a clone is
+  /// decommissioned(deleted)
+  /// @param clone the to-be-decommissioned prototype
   virtual void DecomNotify(cyclus::Agent* clone);
 
-  /**
-     write information about a commodity producer to a stream
-     @param producer the producer
-   */
-  void WriteProducerInformation(cyclus::CommodityProducer*
+  /// write information about a commodity producer to a stream
+  /// @param producer the producer
+  void WriteProducerInformation(cyclus::toolkit::CommodityProducer*
                                 producer);
 };
 } // namespace cycamore 
-#endif
+
+#endif  // CYCAMORE_SRC_MANAGER_INST_H_
