8b10a5c2e28d7213167767c8d1b44eb8835c5344
Anthony Scopatz <scopatz@gmail.com>
2014-05-24 00:19:18 -0500

    updated cycstub from cyclus repo

--- stub_region.h
+++ stub_region.h
@@ -1,90 +1,85 @@
-#ifndef STUB_REGION_H_
-#define STUB_REGION_H_
+#ifndef CYCLUS_STUBS_STUB_REGION_H_
+#define CYCLUS_STUBS_STUB_REGION_H_
 
 #include <string>
 
-#include "context.h"
-#include "region_model.h"
-#include "query_engine.h"
+#include "cyclus.h"
 
 namespace stubs {
 
 /**
-   @class StubRegion 
-    
-   This RegionModel is intended 
-   as a skeleton to guide the implementation of new RegionModel models. 
-    
-   The StubRegion class inherits from the RegionModel class and is 
-   dynamically loaded by the Model class when requested. 
-    
-   @section intro Introduction 
-   Place an introduction to the model here. 
-    
-   @section modelparams Model Parameters 
-   Place a description of the required input parameters which define the 
-   model implementation. 
-    
-   @section optionalparams Optional Parameters 
-   Place a description of the optional input parameters to define the 
-   model implementation. 
-    
-   @section detailed Detailed Behavior 
-   Place a description of the detailed behavior of the model. Consider 
-   describing the behavior at the tick and tock as well as the behavior 
-   upon sending and receiving materials and messages. 
- */
-class StubRegion : public cyclus::RegionModel {
-/* --------------------
- * all REGIONMODEL classes have these members
- * --------------------
- */
+  @class StubRegion
+
+  This Region is intended
+  as a skeleton to guide the implementation of new Region agents.
+
+  The StubRegion class inherits from the Region class and is
+  dynamically loaded by the Agent class when requested.
+
+  @section intro Introduction
+  Place an introduction to the agent here.
+
+  @section agentparams Agent Parameters
+  Place a description of the required input parameters which define the
+  agent implementation.
+
+  @section optionalparams Optional Parameters
+  Place a description of the optional input parameters to define the
+  agent implementation.
+
+  @section detailed Detailed Behavior
+  Place a description of the detailed behavior of the agent. Consider
+  describing the behavior at the tick and tock as well as the behavior
+  upon sending and receiving materials and messages.
+  */
+class StubRegion : public cyclus::Region {
+  /* --------------------
+   * all REGIONMODEL classes have these members
+   * --------------------
+   */
  public:
   /**
-     Constructor for StubRegion Class 
-     @param ctx the cyclus context for access to simulation-wide parameters
-   */
-  StubRegion(cyclus::Context* ctx);
+    Constructor for StubRegion Class
+    @param ctx the cyclus context for access to simulation-wide parameters
+    */
+  explicit StubRegion(cyclus::Context* ctx);
 
   /**
-     every model should be destructable 
-   */
+    every agent should be destructable
+    */
   virtual ~StubRegion();
-    
-  /**
-     Initialize members related to derived module class
 
-     @param qe a pointer to a QueryEngine object containing initialization data
-   */
-  virtual void InitFrom(cyclus::QueryEngine* qe);
-  
   /**
-     Initialize members for a cloned module.
-   */
+    Initialize db with input file info related to derived module class
+    */
+  virtual void InfileToDb(cyclus::InfileTree* qe, cyclus::DbInit di);
+
+  /**
+    Initialize members for a cloned module.
+    */
   virtual void InitFrom(StubRegion* m);
 
   /**
-     A verbose printer for the StubRegion
-   */
-   virtual std::string str();
+    A verbose printer for the StubRegion
+    */
+  virtual std::string str();
 
   /**
-     Initializes a StubRegion object by copying the members of another.
+    Initializes a StubRegion object by copying the members of another.
+    */
+  virtual cyclus::Agent* Clone();
+
+  /* -------------------- */
+
+
+  /* --------------------
+   * all REGIONMODEL classes have these members
+   * --------------------
    */
-   virtual cyclus::Model* Clone();
-  
-/* -------------------- */
 
-
-/* --------------------
- * all REGIONMODEL classes have these members
- * --------------------
- */
-
-/* ------------------- */ 
-
+  /* ------------------- */
 };
 
-} // namespace stubs
-  
-#endif // STUB_REGION_H_
+}  // namespace stubs
+
+#endif  // CYCLUS_STUBS_STUB_REGION_H_
