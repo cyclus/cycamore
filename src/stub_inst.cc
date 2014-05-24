b9b0f7dce5ca3c8a323c5217318b529f132a8f8e
Anthony Scopatz <scopatz@gmail.com>
2014-05-24 00:19:18 -0500

    updated cycstub from cyclus repo

--- src/stub_inst.cc
+++ src/stub_inst.cc
@@ -2,44 +2,53 @@
 
 using stubs::StubInst;
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 StubInst::StubInst(cyclus::Context* ctx)
-    : cyclus::InstModel(ctx),
-      cyclus::Model(ctx)
-    {};
+    : cyclus::Institution(ctx) {};
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-StubInst::~StubInst() {};
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+StubInst::~StubInst() {}
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
-void StubInst::InitFrom(cyclus::QueryEngine* qe) {
-  cyclus::InstModel::InitFrom(qe);
-  qe = qe->QueryElement(ModelImpl());
-  
-  //retrieve input data members here. For example :  
-  //string query = "incommodity";
-  //incommodity_ = lexical_cast<double>(qe->getElementContent(query));
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void StubInst::InfileToDb(cyclus::InfileTree* qe, cyclus::DbInit di) {
+  Agent::InfileToDb(qe, di);
+  qe = qe->SubTree(spec());
+  // retrieve input data members here. For example:
+  //
+  //   int cycle_len = lexical_cast<int>(input->getElementContent("cycle_length"));
+  //   ...
+  //   di.NewDatum("StubFacilityParams")
+  //     ->AddVal("cycle_length", cycle_len)
+  //     ...
+  //     ->Record();
 }
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-cyclus::Model* StubInst::Clone() {
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+cyclus::Agent* StubInst::Clone() {
   StubInst* m = new StubInst(context());
   m->InitFrom(this);
   return m;
 }
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void StubInst::InitFrom(StubInst* m) {
-  cyclus::InstModel::InitFrom(m);
+  cyclus::Institution::InitFrom(m);
   // Initialize stubinst members for a cloned module here
 }
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 std::string StubInst::str() {
-  return InstModel::str();
-};
+  return Institution::str();
+}
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-extern "C" cyclus::Model* ConstructStubInst(cyclus::Context* ctx) {
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+extern "C" cyclus::Agent* ConstructStubInst(cyclus::Context* ctx) {
   return new StubInst(ctx);
 }
+
+// required to get functionality in cyclus agent unit tests library
+#ifndef CYCLUS_AGENT_TESTS_CONNECTED
+int ConnectAgentTests();
+static int cyclus_agent_tests_connected = ConnectAgentTests();
+#define CYCLUS_AGENT_TESTS_CONNECTED cyclus_agent_tests_connected
+#endif // CYCLUS_AGENT_TESTS_CONNECTED
