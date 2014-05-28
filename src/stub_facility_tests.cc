e248c3dd558fe1dd37ee84b9992fb169ffa72ca9
Anthony Scopatz <scopatz@gmail.com>
2014-05-27 21:09:33 -0500

    updated stubs to work

--- src/stub_facility_tests.cc
+++ src/stub_facility_tests.cc
@@ -46,14 +46,14 @@ TEST_F(StubFacilityTest, ReceiveMessage) {
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 TEST_F(StubFacilityTest, Tick) {
   int time = 1;
-  ASSERT_NO_THROW(src_facility_->Tick(time));
+  ASSERT_NO_THROW(src_facility_->Tick());
   // Test StubFacility specific behaviors of the Tick function here
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 TEST_F(StubFacilityTest, Tock) {
   int time = 1;
-  EXPECT_NO_THROW(src_facility_->Tock(time));
+  EXPECT_NO_THROW(src_facility_->Tock());
   // Test StubFacility specific behaviors of the Tock function here
 }
 
@@ -61,6 +61,13 @@ TEST_F(StubFacilityTest, Tock) {
 cyclus::Agent* StubFacilityConstructor(cyclus::Context* ctx) {
   return new StubFacility(ctx);
 }
+
+// required to get functionality in cyclus agent unit tests library
+#ifndef CYCLUS_AGENT_TESTS_CONNECTED
+int ConnectAgentTests();
+static int cyclus_agent_tests_connected = ConnectAgentTests();
+#define CYCLUS_AGENT_TESTS_CONNECTED cyclus_agent_tests_connected
+#endif // CYCLUS_AGENT_TESTS_CONNECTED
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 INSTANTIATE_TEST_CASE_P(StubFac, FacilityTests,
--- src/stub_inst.cc
+++ src/stub_inst.cc
@@ -10,33 +10,6 @@ StubInst::StubInst(cyclus::Context* ctx)
 StubInst::~StubInst() {}
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-void StubInst::InfileToDb(cyclus::InfileTree* qe, cyclus::DbInit di) {
-  Agent::InfileToDb(qe, di);
-  qe = qe->SubTree(spec());
-  // retrieve input data members here. For example:
-  //
-  //   int cycle_len = lexical_cast<int>(input->getElementContent("cycle_length"));
-  //   ...
-  //   di.NewDatum("StubFacilityParams")
-  //     ->AddVal("cycle_length", cycle_len)
-  //     ...
-  //     ->Record();
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-cyclus::Agent* StubInst::Clone() {
-  StubInst* m = new StubInst(context());
-  m->InitFrom(this);
-  return m;
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-void StubInst::InitFrom(StubInst* m) {
-  cyclus::Institution::InitFrom(m);
-  // Initialize stubinst members for a cloned module here
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 std::string StubInst::str() {
   return Institution::str();
 }
@@ -45,10 +18,3 @@ std::string StubInst::str() {
 extern "C" cyclus::Agent* ConstructStubInst(cyclus::Context* ctx) {
   return new StubInst(ctx);
 }
-
-// required to get functionality in cyclus agent unit tests library
-#ifndef CYCLUS_AGENT_TESTS_CONNECTED
-int ConnectAgentTests();
-static int cyclus_agent_tests_connected = ConnectAgentTests();
-#define CYCLUS_AGENT_TESTS_CONNECTED cyclus_agent_tests_connected
-#endif // CYCLUS_AGENT_TESTS_CONNECTED
