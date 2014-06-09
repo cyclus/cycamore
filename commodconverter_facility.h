907f3f2edceadab625750da8fca83d2ad89a2734
Katy Huff <katyhuff@gmail.com>
2014-06-08 18:36:19 -0700

    converted schema to new pragma system.

--- commodconverter_facility.h
+++ commodconverter_facility.h
@@ -5,9 +5,13 @@
 
 #include "cyclus.h"
 
+// forward declaration
 namespace commodconverter {
+class CommodconverterFacility;
+} // namespace commodconverter
 
 
+namespace commodconverter {
 /// @class CommodconverterFacility
 ///
 /// This Facility is intended
@@ -31,11 +35,13 @@ namespace commodconverter {
 /// Place a description of the detailed behavior of the agent. Consider
 /// describing the behavior at the tick and tock as well as the behavior
 /// upon sending and receiving materials and messages.
-class CommodconverterFacility : public cyclus::Facility  {
+class CommodconverterFacility 
+  : public cyclus::Facility,
+    public cyclus::toolkit::CommodityProducer {
  public:  
   /// Constructor for CommodconverterFacility Class
   /// @param ctx the cyclus context for access to simulation-wide parameters
-  explicit CommodconverterFacility(cyclus::Context* ctx);
+  CommodconverterFacility(cyclus::Context* ctx);
 
   /// The Prime Directive
   /// Generates code that handles all input file reading and restart operations
@@ -43,10 +49,10 @@ class CommodconverterFacility : public cyclus::Facility  {
   /// @warning The Prime Directive must have a space before it! (A fix will be
   /// in 2.0 ^TM)
   
-  #pragma cyclus
+  #pragma cyclus decl
 
-  #pragma cyclus note {"doc": "A commodconverter facility is provided as a skeleton " \
-                              "for the design of new facility agents."}
+  #pragma cyclus note {"doc": "A commodconverter facility converts from one " \
+                              "commodity to another, with an optional delay."}
 
   /// A verbose printer for the CommodconverterFacility
   virtual std::string str();
@@ -59,7 +65,32 @@ class CommodconverterFacility : public cyclus::Facility  {
   /// @param time the time of the tock
   virtual void Tock();
 
-  // and away we go!
+  /* --- Module Members --- */
+
+  #pragma cyclus var {"tooltip":"input commodity",\
+                      "doc":"commodity accepted by this facility"}
+  std::string in_commod;
+  #pragma cyclus var {"tooltip":"output commodity",\
+                      "doc":"commodity produced by this facility"}
+  std::string out_commod;
+  #pragma cyclus var {"tooltip":"input recipe",\
+                      "doc":"recipe accpeted by this facility"}
+  std::string in_recipe;
+  #pragma cyclus var {"tooltip":"output recipe",\
+                      "doc":"recipe produced by this facility"}
+  std::string out_recipe;
+  #pragma cyclus var {"default": 0,\
+                      "tooltip":"process time",\
+                      "doc":"the time it takes to convert a received commodity."}
+  int process_time; //should be nonnegative
+  #pragma cyclus var {"default": 1e299,\
+                      "tooltip":"maximum inventory size",\
+                      "doc":"the amount of material that can be in storage at "\
+                      "one time."}
+  double max_inv_size; //should be nonnegative
+  #pragma cyclus var{'capacity': 'max_inv_size'}
+  
+  friend class CommodconverterFacilityTest;
 };
 
 }  // namespace commodconverter
