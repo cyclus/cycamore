c8909092fd7ddd107ba371a668661f2640405ea0
Katy Huff <katyhuff@gmail.com>
2014-07-13 18:36:53 -0700

    base state vars and input vars. need matrix behavior

--- src/separationmatrix_facility.cc
+++ src/separationmatrix_facility.cc
@@ -17,6 +17,33 @@ void SeparationmatrixFacility::Tick() {}
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void SeparationmatrixFacility::Tock() {}
 
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+const double SeparationmatrixFacility::inventory_quantity(std::string commod)const {
+  using cyclus::toolkit::ResourceBuff;
+
+  std::map<std::string, ResourceBuff>::const_iterator found;
+  found = inventory.find(commod);
+  double amt;
+  if ( found != inventory.end() ){
+    amt = (*found).second.quantity();
+  } else {
+    amt =0;
+  }
+  return amt;
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+const double SeparationmatrixFacility::inventory_quantity() const {
+  using cyclus::toolkit::ResourceBuff;
+
+  double total = 0;
+  std::map<std::string, ResourceBuff>::const_iterator it;
+  for( it = inventory.begin(); it != inventory.end(); ++it) {
+    total += inventory_quantity((*it).first);
+  }
+  return total;
+}
+
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 extern "C" cyclus::Agent* ConstructSeparationmatrixFacility(cyclus::Context* ctx) {
   return new SeparationmatrixFacility(ctx);
--- src/separationmatrix_facility.h
+++ src/separationmatrix_facility.h
@@ -84,7 +84,70 @@ class SeparationmatrixFacility : public cyclus::Facility  {
   /// @param time the time of the tock
   virtual void Tock();
 
-  // and away we go!
+  // @brief gives current quantity of commod in inventory
+  const double inventory_quantity(std::string commod) const;
+
+  /// @brief gives current quantity of all commods in inventory
+  const double inventory_quantity() const;
+
+  /// @brief this facility's commodity-recipe context
+  inline void crctx(const cyclus::toolkit::CommodityRecipeContext& crctx) {
+    crctx_ = crctx;
+  }
+  inline cyclus::toolkit::CommodityRecipeContext crctx() const {
+    return crctx_;
+  }
+
+  /// @brief returns the time key for ready materials
+  int ready(){ return context()->time() - process_time ; }
+
+  /* --- Module Members --- */
+  #pragma cyclus var {"tooltip":"input commodity",\
+                      "doc":"commodity accepted by this facility"}
+  std::string in_commod;
+  inline std::string in_commod_() const {return in_commod;};
+
+  #pragma cyclus var {"tooltip":"output stream list     ",\
+                      "doc":"list of commodities produced by this facility"}
+  std::vector< std::string > out_commods;
+  inline std::vector< std::string > out_commods_() const {return out_commods;};
+
+  #pragma cyclus var {"default": 0,\
+                      "tooltip":"process time (timesteps)",\
+                      "doc":"the time it takes to convert a received commodity (timesteps)."}
+  int process_time; //should be nonnegative
+
+  #pragma cyclus var {"default": 1e299,\
+                      "tooltip":"maximum inventory size (kg)",\
+                      "doc":"the amount of material that can be in storage at "\
+                      "one time (kg)."}
+  double max_inv_size; //should be nonnegative
+
+  #pragma cyclus var{'capacity': 'max_inv_size'}
+  std::map<std::string, cyclus::toolkit::ResourceBuff> inventory;
+  cyclus::toolkit::ResourceBuff stocks;
+  cyclus::toolkit::ResourceBuff wastes;
+
+  /// @brief a list of preffered commodities
+  std::map<int, std::set<std::string> > prefs_;
+
+  /// @brief map from ready time to resource buffers
+  std::map<int, std::map<std::string,
+    cyclus::toolkit::ResourceBuff> > processing;
+
+  cyclus::toolkit::CommodityRecipeContext crctx_;
+
+  /// @brief the processing time required for a full process
+  inline void process_time_(int t) { process_time = t; }
+  inline int process_time_() const { return process_time; }
+
+  /// @brief the maximum amount allowed in inventory
+  inline void capacity(double c) { max_inv_size = c; }
+  inline double capacity() const { return max_inv_size; }
+
+  /// @brief current maximum amount that can be added to processing
+  inline double current_capacity() const {
+    return (max_inv_size - inventory_quantity()); } 
 };
 
 }  // namespace separationmatrix
