cfeffc4569cec8ac6c5a309d9f3b119f624ff417
Katy Huff <katyhuff@gmail.com>
2014-08-24 18:13:24 -0700

    fleshes out sepmatrix tests

--- src/separationmatrix_tests.cc
+++ src/separationmatrix_tests.cc
@@ -1,41 +1,128 @@
 #include <gtest/gtest.h>
 
-#include "separationmatrix.h"
+#include "separationmatrix_tests.h"
 
-#include "context.h"
-#include "facility_tests.h"
-#include "agent_tests.h"
-
-using separationmatrix::SeparationMatrix;
+namespace separationmatrix {
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-class SeparationMatrixTest : public ::testing::Test {
- protected:
-  cyclus::TestContext tc_;
-  SeparationMatrix* src_facility_;
+void SeparationMatrixTest::SetUp() {
+  src_facility_ = new SeparationMatrix(tc_.get());
+  InitParameters();
+  SetUpSeparationMatrix();
+}
 
-  virtual void SetUp() {
-    src_facility_ = new SeparationMatrix(tc_.get());
-  }
+void SeparationMatrixTest::TearDown() {
+  delete src_facility_;
+}
 
-  virtual void TearDown() {}
-};
+void SeparationMatrixTest::InitParameters(){
+  in_c1 = "in_c1";
+  out_c1 = "out_c1";
+  in_r1 = "in_r1";
+  out_r1 = "out_r1";
+  process_time = 10;
+  max_inv_size = 200;
+  capacity = 20;
+  cost = 1;
+
+  cyclus::CompMap v;
+  v[922350000] = 1;
+  v[922380000] = 2;
+  cyclus::Composition::Ptr recipe = cyclus::Composition::CreateFromAtom(v);
+  tc_.get()->AddRecipe(in_r1, recipe);
+
+  v[94239] = 0.25;
+  recipe = cyclus::Composition::CreateFromAtom(v);
+  tc_.get()->AddRecipe(out_r1, recipe);
+}
+
+void SeparationMatrixTest::SetUpSeparationMatrix(){
+  src_facility_->in_commod_(in_c1);
+  src_facility_->out_commod_(out_c1);
+  src_facility_->in_recipe_(in_r1);
+  src_facility_->out_recipe_(out_r1);
+  src_facility_->process_time_(process_time);
+  src_facility_->max_inv_size_(max_inv_size);
+  src_facility_->capacity_(capacity);
+  src_facility_->cost_(cost);
+}
+
+void SeparationMatrixTest::TestInitState(SeparationMatrix* fac){
+  EXPECT_EQ(process_time, fac->process_time_());
+  EXPECT_EQ(max_inv_size, fac->max_inv_size_());
+  EXPECT_EQ(capacity, fac->capacity_());
+  EXPECT_EQ(out_c1, fac->out_commod_());
+  EXPECT_EQ(out_r1, fac->out_recipe_());
+  EXPECT_EQ(in_c1, fac->in_commod_());
+  EXPECT_EQ(in_r1, fac->in_recipe_());
+  EXPECT_EQ(cost, fac->cost_());
+}
+
+void SeparationMatrixTest::TestRequest(SeparationMatrix* fac, double cap){
+  cyclus::Material::Ptr req = fac->Request_();
+  EXPECT_EQ(cap, req->quantity());
+}
+
+void SeparationMatrixTest::TestAddMat(SeparationMatrix* fac, 
+    cyclus::Material::Ptr mat){
+  double amt = mat->quantity();
+  double before = fac->inventory.quantity();
+  fac->AddMat_(mat);
+  double after = fac->inventory.quantity();
+  EXPECT_EQ(amt, after - before);
+}
+
+void SeparationMatrixTest::TestBuffers(SeparationMatrix* fac, double inv, 
+    double proc, double stocks){
+  double t = tc_.get()->time();
+
+  EXPECT_EQ(inv, fac->inventory.quantity());
+  EXPECT_EQ(proc, fac->processing[t].quantity());
+  EXPECT_EQ(stocks, fac->stocks.quantity());
+}
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 TEST_F(SeparationMatrixTest, clone) {
   SeparationMatrix* cloned_fac =
       dynamic_cast<SeparationMatrix*> (src_facility_->Clone());
+  TestInitState(cloned_fac);
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 TEST_F(SeparationMatrixTest, InitialState) {
   // Test things about the initial state of the facility here
+  TestInitState(src_facility_);
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+TEST_F(SeparationMatrixTest, CurrentCapacity) {
+  EXPECT_EQ(capacity, src_facility_->current_capacity());
+  src_facility_->max_inv_size_(1e299);
+  EXPECT_EQ(1e299, src_facility_->max_inv_size_());
+  EXPECT_EQ(capacity, src_facility_->capacity_());
+  EXPECT_EQ(capacity, src_facility_->current_capacity());
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 TEST_F(SeparationMatrixTest, Print) {
   EXPECT_NO_THROW(std::string s = src_facility_->str());
   // Test SeparationMatrix specific aspects of the print method here
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+TEST_F(SeparationMatrixTest, Request) { 
+  TestRequest(src_facility_, src_facility_->current_capacity());
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+TEST_F(SeparationMatrixTest, AddMats) { 
+  double cap = src_facility_->current_capacity();
+  cyclus::Material::Ptr mat = cyclus::NewBlankMaterial(0.5*cap);
+  TestAddMat(src_facility_, mat);
+
+  cyclus::Composition::Ptr rec = tc_.get()->GetRecipe(in_r1);
+  cyclus::Material::Ptr recmat = cyclus::Material::CreateUntracked(0.5*cap, rec);
+  TestAddMat(src_facility_, recmat);
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -46,13 +133,69 @@ TEST_F(SeparationMatrixTest, Tick) {
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 TEST_F(SeparationMatrixTest, Tock) {
+
+  // initially, nothing in the buffers
+  TestBuffers(src_facility_,0,0,0);
+
+  double cap = src_facility_->current_capacity();
+  cyclus::Composition::Ptr rec = tc_.get()->GetRecipe(in_r1);
+  cyclus::Material::Ptr mat = cyclus::Material::CreateUntracked(cap, rec);
+  TestAddMat(src_facility_, mat);
+
+  // affter add, the inventory has the material
+  TestBuffers(src_facility_,cap,0,0);
+
   EXPECT_NO_THROW(src_facility_->Tock());
-  // Test SeparationMatrix specific behaviors of the Tock function here
+
+  // after tock, the processing buffer has the material
+  TestBuffers(src_facility_,0,cap,0);
+
+  EXPECT_EQ(0, tc_.get()->time());
+  for( int i = 1; i < process_time-1; ++i){
+    tc_.get()->time(i);
+    EXPECT_NO_THROW(src_facility_->Tock());
+    TestBuffers(src_facility_,0,0,0);
+  }
+  
+  tc_.get()->time(process_time);
+  EXPECT_EQ(process_time, tc_.get()->time());
+  EXPECT_EQ(0, src_facility_->ready());
+  src_facility_->Tock();
+  TestBuffers(src_facility_,0,0,cap);
+
+  tc_.get()->time(process_time+1);
+  EXPECT_EQ(1, src_facility_->ready());
+  src_facility_->Tock();
+  TestBuffers(src_facility_,0,0,cap);
+
 }
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+TEST_F(SeparationMatrixTest, NoProcessTime) {
+  // tests what happens when the process time is zero
+  src_facility_->process_time_(0);
+  EXPECT_EQ(0, src_facility_->process_time_());
+
+  double cap = src_facility_->current_capacity();
+  cyclus::Composition::Ptr rec = tc_.get()->GetRecipe(in_r1);
+  cyclus::Material::Ptr mat = cyclus::Material::CreateUntracked(cap, rec);
+  TestAddMat(src_facility_, mat);
+
+  // affter add, the inventory has the material
+  TestBuffers(src_facility_,cap,0,0);
+
+  EXPECT_NO_THROW(src_facility_->Tock());
+
+  // affter tock, the stocks have the material
+  TestBuffers(src_facility_,0,0,cap);
+}
+
+
+} // namespace separationmatrix
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 cyclus::Agent* SeparationMatrixConstructor(cyclus::Context* ctx) {
-  return new SeparationMatrix(ctx);
+  return new separationmatrix::SeparationMatrix(ctx);
 }
 
 // required to get functionality in cyclus agent unit tests library
