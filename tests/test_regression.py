28f25e08ddd977a3500f71e841a1c48b0a70bb0d
Matthew Gidden <matthew.gidden@gmail.com>
2015-01-05 15:48:29 -0600

    moved dynamic capacitated test into regression tests

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -187,4 +187,110 @@ class TestPhysorSources(TestRegression):
                                  xa["SenderId"] == self.suox)] 
         obs[rows["Time"]] = [self.rsrc_qtys[x] for x in rows["ResourceId"]]
         np.testing.assert_almost_equal(uox_exp, obs)
-         
+
+class TestDynamicCapacitated(TestRegression):
+    """Tests dynamic capacity restraints involving changes in the number of
+    source and sink facilities.
+
+    A source facility is expected to offer a commodity of amount 1,
+    and a sink facility is expected to request for a commodity of amount 1.
+    Therefore, number of facilities correspond to the amounts of offers
+    and requests.
+
+    At time step 1, 3 source facilities and 2 sink facilities are deployed, and
+    at time step 2, additional 2 sink facilities are deployed. After time
+    step 2, the older 2 sink facilities are decommissioned.
+    According to this deployment schedule, at time step 1, only 2 transactions
+    are expected, the number of sink facilities being the constraint; whereas,
+    at time step 2, only 3 transactions are expected, the number of source
+    facilities being the constraint. At time step 3, after decommissioning 2
+    older sink facilities, the remaining number of sink facilities becomes
+    the constraint, resulting in the same transaction amount as in time step 1.
+    """
+    def __init__(self):
+        super(TestDynamicCapacitated, self).__init__()
+        self.inf = "./input/dynamic_capacitated.xml"
+
+    def setup(self):
+        super(TestDynamicCapacitated, self).setup()
+                
+        # Find agent ids of source and sink facilities
+        self.agent_ids = self.agent_entry["AgentId"]
+        self.agent_impl = self.agent_entry["Spec"]
+        self.depl_time = self.agent_entry["EnterTime"]
+        self.exit_time = self.agent_exit["ExitTime"]
+        self.exit_ids = self.agent_exit["AgentId"]
+        self.source_id = find_ids(":agents:Source", self.agent_impl, self.agent_ids)
+        self.sink_id = find_ids(":agents:Sink", self.agent_impl, self.agent_ids)
+
+        # Check transactions
+        self.sender_ids = self.transactions["SenderId"]
+        self.receiver_ids = self.transactions["ReceiverId"]
+        self.trans_time = self.transactions["Time"]
+        self.trans_resource = self.transactions["ResourceId"]
+
+        # Track transacted resources
+        self.resource_ids = self.resources["ResourceId"]
+        self.quantities = self.resources["Quantity"]
+
+    def test_source_deployment(self):
+        # test number of sources
+        assert_equal(len(self.source_id), 3)
+        # Test that source facilities are all deployed at time step 1
+        for s in self.source_id:
+            assert_equal(self.depl_time[np.where(self.agent_ids == s)], 1)
+
+    def test_sink_deployment(self):
+        # test number of sinks
+        assert_equal(len(self.sink_id), 4)
+        # Test that first 2 sink facilities are deployed at time step 1
+        # and decommissioned at time step 2
+        for i in [0, 1]:
+            assert_equal(self.depl_time[np.where(self.agent_ids == self.sink_id[i])], 1)
+            assert_equal(self.exit_time[np.where(self.exit_ids == self.sink_id[i])], 2)
+        # Test that second 2 sink facilities are deployed at time step 2
+        # and decommissioned at time step 3
+        for i in [2, 3]:
+            assert_equal(self.depl_time[np.where(self.agent_ids == self.sink_id[i])], 2)
+            assert_equal(self.exit_time[np.where(self.exit_ids == self.sink_id[i])], 3)
+
+    def test_xaction_general(self):        
+        # Check that transactions are between sources and sinks only
+        for s in self.sender_ids:
+            assert_equal(len(np.where(self.source_id == s)[0]), 1)
+        for r in self.receiver_ids:
+            assert_equal(len(np.where(self.sink_id == r)[0]), 1)
+        # Total expected number of transactions
+        assert_equal(len(self.trans_time), 7)
+        # Check that at time step 1, there are 2 transactions
+        assert_equal(len(np.where(self.trans_time == 1)[0]), 2)
+        # Check that at time step 2, there are 3 transactions
+        assert_equal(len(np.where(self.trans_time == 2)[0]), 3)
+        # Check that at time step 3, there are 2 transactions
+        assert_equal(len(np.where(self.trans_time == 3)[0]), 2)
+
+    def test_xaction_specific(self):                
+        # Check that at time step 1, there are 2 transactions with total
+        # amount of 2
+        quantity = 0
+        for t in np.where(self.trans_time == 1)[0]:
+            quantity += self.quantities[
+                np.where(self.resource_ids == self.trans_resource[t])]
+        assert_equal(quantity, 2)
+
+        # Check that at time step 2, there are 3 transactions with total
+        # amount of 3
+        quantity = 0
+        for t in np.where(self.trans_time == 2)[0]:
+            quantity += self.quantities[
+                np.where(self.resource_ids == self.trans_resource[t])]
+        assert_equal(quantity, 3)
+
+        # Check that at time step 3, there are 2 transactions with total
+        # amount of 2
+        quantity = 0
+        for t in np.where(self.trans_time == 3)[0]:
+            quantity += self.quantities[
+                np.where(self.resource_ids == self.trans_resource[t])]
+        assert_equal(quantity, 2)
+
