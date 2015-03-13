e9b44b357c74687ca27ac90dce134ecdf25da018
Robert Carlsen <rwcarlsen@gmail.com>
2015-03-13 10:58:08 -0500

    skeleton for deploy inst tests

--- src/deploy_inst_tests.cc
+++ src/deploy_inst_tests.cc
@@ -5,27 +5,27 @@
 #include "institution_tests.h"
 #include "agent_tests.h"
 
+TEST(DeployInstTests, ProtoNames) {
+}
+
+TEST(DeployInstTests, BuildTimes) {
+}
+
+TEST(DeployInstTests, FiniteLifetimes) {
+}
+
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// required to get functionality in cyclus agent unit tests library
 cyclus::Agent* DeployInstitutionConstructor(cyclus::Context* ctx) {
   return new cycamore::DeployInst(ctx);
 }
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-class DeployInstTest : public ::testing::Test {
- protected:
-  virtual void SetUp() {}
-
-  virtual void TearDown() {}
-};
-
-// required to get functionality in cyclus agent unit tests library
 #ifndef CYCLUS_AGENT_TESTS_CONNECTED
 int ConnectAgentTests();
 static int cyclus_agent_tests_connected = ConnectAgentTests();
 #define CYCLUS_AGENT_TESTS_CONNECTED cyclus_agent_tests_connected
 #endif  // CYCLUS_AGENT_TESTS_CONNECTED
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 INSTANTIATE_TEST_CASE_P(DeployInst, InstitutionTests,
                         Values(&DeployInstitutionConstructor));
 INSTANTIATE_TEST_CASE_P(DeployInst, AgentTests,
