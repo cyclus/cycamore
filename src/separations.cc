4139b4e0aa9eae39f63d85751b70f4502d6d96de
Robert Carlsen <rwcarlsen@gmail.com>
2015-03-25 12:15:39 -0500

    skeleton of separations facility

---
+++ src/separations.cc
@@ -0,0 +1,198 @@
+#include "separations.h"
+
+using cyclus::Material;
+using cyclus::Composition;
+using cyclus::toolkit::ResBuf;
+using cyclus::toolkit::MatVec;
+using cyclus::KeyError;
+using cyclus::ValueError;
+using cyclus::Request;
+
+namespace cycamore {
+
+Separations::Separations(cyclus::Context* ctx)
+    : cyclus::Facility(ctx) {
+  cyclus::Warn<cyclus::EXPERIMENTAL_WARNING>("the Separations archetype "
+                                             "is experimental");
+}
+
+cyclus::Inventories Separations::SnapshotInv() {
+  cyclus::Inventories invs;
+
+  invs["leftover"] = leftover.PopNRes(leftover.count());
+  leftover.Push(invs["leftover"]);
+  invs["feed"] = feed.PopNRes(feed.count());
+  feed.Push(invs["feed"]);
+
+  std::map<std::string, ResBuf<Material> >::iterator it;
+  for (it = streambufs.begin(); it != streambufs.end(); ++it) {
+    invs[it->first] = it->second.PopNRes(it->second.count());
+    it->second.Push(invs[it->first]);
+  }
+
+  return invs;
+}
+void Separations::InitInv(cyclus::Inventories& inv) {
+  leftover.Push(inv["leftover"]);
+  feed.Push(inv["feed"]);
+
+  cyclus::Inventories::iterator it;
+  for (it = inv.begin(); it != inv.end(); ++it) {
+    streambufs[it->first].Push(it->second);
+  }
+}
+
+typedef std::map<std::string, std::pair<double, std::map<int, double> > > StreamSet;
+typedef std::pair<double, std::map<int, double> > Stream;
+
+void Separations::EnterNotify() {
+  cyclus::Facility::EnterNotify();
+  StreamSet::iterator it;
+  for (it = streams_.begin(); it != streams_.end(); ++it) {
+    std::string name = it->first;
+    Stream stream = it->second;
+    double cap = stream.first;
+    if (cap >= 0) {
+      streambufs[name].capacity(cap);
+    }
+  }
+}
+
+void Separations::Tick() {
+}
+
+std::set<cyclus::RequestPortfolio<Material>::Ptr>
+Separations::GetMatlRequests() {
+  //using cyclus::RequestPortfolio;
+
+  //std::set<RequestPortfolio<Material>::Ptr> ports;
+  //Material::Ptr m;
+
+  //int n_assem_order = n_assem_core - core.count()
+  //                    + n_assem_fresh - fresh.count();
+  //if (n_assem_order == 0) {
+  //  return ports;
+  //}
+
+  //for (int i = 0; i < n_assem_order; i++) {
+  //  RequestPortfolio<Material>::Ptr port(new RequestPortfolio<Material>());
+  //  std::vector<Request<Material>*> mreqs;
+  //  for (int j = 0; j < fuel_incommods.size(); j++) {
+  //    std::string commod = fuel_incommods[j];
+  //    double pref = fuel_prefs[j];
+  //    Composition::Ptr recipe = context()->GetRecipe(fuel_inrecipes[j]);
+  //    m = Material::CreateUntracked(assem_size, recipe);
+  //    Request<Material>* r = port->AddRequest(m, this, commod, pref, true);
+  //    mreqs.push_back(r);
+  //  }
+  //  port->AddMutualReqs(mreqs);
+  //  ports.insert(port);
+  //}
+
+  //return ports;
+}
+
+void Separations::GetMatlTrades(
+    const std::vector< cyclus::Trade<Material> >& trades,
+    std::vector<std::pair<cyclus::Trade<Material>,
+    Material::Ptr> >& responses) {
+  //using cyclus::Trade;
+
+  //std::map<std::string, MatVec> mats = PopSpent();
+  //std::vector< cyclus::Trade<cyclus::Material> >::const_iterator it;
+  //for (int i = 0; i < trades.size(); i++) {
+  //  std::string commod = trades[i].request->commodity();
+  //  Material::Ptr m = mats[commod].back();
+  //  mats[commod].pop_back();
+  //  responses.push_back(std::make_pair(trades[i], m));
+  //  res_indexes.erase(m->obj_id());
+  //}
+  //PushSpent(mats); // return leftovers back to spent buffer
+}
+
+void Separations::AcceptMatlTrades(
+    const std::vector< std::pair<cyclus::Trade<Material>,
+    Material::Ptr> >& responses) {
+
+  //std::vector< std::pair<cyclus::Trade<cyclus::Material>,
+  //                       cyclus::Material::Ptr> >::const_iterator trade;
+
+  //std::stringstream ss;
+  //int nload = std::min((int)responses.size(), n_assem_core - core.count());
+  //if (nload > 0) {
+  //  ss << nload << " assemblies";
+  //  Record("LOAD", ss.str());
+  //}
+
+  //for (trade = responses.begin(); trade != responses.end(); ++trade) {
+  //  std::string commod = trade->first.request->commodity();
+  //  Material::Ptr m = trade->second;
+  //  index_res(m, commod);
+
+  //  if (core.count() < n_assem_core) {
+  //    core.Push(m);
+  //  } else {
+  //    fresh.Push(m);
+  //  }
+  //}
+}
+
+std::set<cyclus::BidPortfolio<Material>::Ptr>
+Separations::GetMatlBids(cyclus::CommodMap<Material>::type&
+                          commod_requests) {
+  //using cyclus::BidPortfolio;
+
+  //std::set<BidPortfolio<Material>::Ptr> ports;
+
+  //bool gotmats = false;
+  //std::map<std::string, MatVec> all_mats;
+  //for (int i = 0; i < fuel_outcommods.size(); i++) {
+  //  std::string commod = fuel_outcommods[i];
+  //  std::vector<Request<Material>*>& reqs = commod_requests[commod];
+  //  if (reqs.size() == 0) {
+  //    continue;
+  //  } else if (!gotmats) {
+  //    all_mats = PeekSpent();
+  //  }
+
+  //  MatVec mats = all_mats[commod];
+  //  if (mats.size() == 0) {
+  //    continue;
+  //  }
+
+  //  BidPortfolio<Material>::Ptr port(new BidPortfolio<Material>());
+
+  //  for (int j = 0; j < reqs.size(); j++) {
+  //    Request<Material>* req = reqs[j];
+  //    double tot_bid = 0;
+  //    for (int k = 0; k < mats.size(); k++) {
+  //      Material::Ptr m = mats[k];
+  //      tot_bid += m->quantity();
+  //      port->AddBid(req, m, this, true);
+  //      if (tot_bid >= req->target()->quantity()) {
+  //        break;
+  //      }
+  //    }
+  //  }
+
+  //  double tot_qty = 0;
+  //  for (int j = 0; j < mats.size(); j++) {
+  //    tot_qty += mats[j]->quantity();
+  //  }
+  //  cyclus::CapacityConstraint<Material> cc(tot_qty);
+  //  port->AddConstraint(cc);
+  //  ports.insert(port);
+  //}
+
+  //return ports;
+}
+
+void Separations::Tock() {
+}
+
+extern "C" cyclus::Agent* ConstructSeparations(cyclus::Context* ctx) {
+  return new Separations(ctx);
+}
+
+} // namespace cycamore
+
---
+++ src/separations.h
@@ -0,0 +1,82 @@
+#ifndef CYCAMORE_SRC_SEPARATIONS_H_
+#define CYCAMORE_SRC_SEPARATIONS_H_
+
+#include "cyclus.h"
+
+namespace cycamore {
+
+class Separations : public cyclus::Facility {
+ public:
+  Separations(cyclus::Context* ctx);
+  virtual ~Separations(){};
+
+  virtual void Tick();
+  virtual void Tock();
+  virtual void EnterNotify();
+
+  virtual void AcceptMatlTrades(const std::vector<std::pair<
+      cyclus::Trade<cyclus::Material>, cyclus::Material::Ptr> >& responses);
+
+  virtual std::set<cyclus::RequestPortfolio<cyclus::Material>::Ptr>
+  GetMatlRequests();
+
+  virtual std::set<cyclus::BidPortfolio<cyclus::Material>::Ptr> GetMatlBids(
+      cyclus::CommodMap<cyclus::Material>::type& commod_requests);
+
+  virtual void GetMatlTrades(
+      const std::vector<cyclus::Trade<cyclus::Material> >& trades,
+      std::vector<std::pair<cyclus::Trade<cyclus::Material>,
+                            cyclus::Material::Ptr> >& responses);
+
+  #pragma cyclus clone
+  #pragma cyclus initfromcopy
+  #pragma cyclus infiletodb
+  #pragma cyclus initfromdb
+  #pragma cyclus schema
+  #pragma cyclus annotations
+  #pragma cyclus snapshot
+
+  virtual cyclus::Inventories SnapshotInv();
+  virtual void InitInv(cyclus::Inventories& inv);
+
+  // the following pragmas are ommitted and generated manually to handle a
+  // vector of resource buffers:
+  //     #pragma cyclus snapshotinv
+  //     #pragma cyclus initinv
+
+ private:
+  #pragma cyclus var { \
+    "alias": ["streams", "name", ["info", "cap", ["efficiencies", "comp", "eff"]]], \
+    "uitype": ["oneormore", "string", ["pair", "double", ["oneormore", "nuclide", "double"]]], \
+    "doc": "Output streams for separations.  Each stream must have a unique name," \
+           " a max buffer capacity in kg (neg values indicate infinite size), and a set of component efficiencies." \
+           " 'comp' is a component to be separated into this stream (e.g. U, Pu, etc.) and 'eff' is the mass fraction of that component that is separated from the feed into this output stream.", \
+  }
+  std::map<std::string,std::pair<double,std::map<int,double> > > streams_;
+
+  #pragma cyclus var { \
+    "doc" : "", \
+  }
+  double feedbuf_size;
+
+  #pragma cyclus var { \
+    "capacity" : "feedbuf_size", \
+  }
+  cyclus::toolkit::ResBuf<cyclus::Material> feed;
+
+  #pragma cyclus var { \
+    "doc" : "", \
+  }
+  double leftoverbuf_size;
+
+  #pragma cyclus var { \
+    "capacity" : "leftoverbuf_size", \
+  }
+  cyclus::toolkit::ResBuf<cyclus::Material> leftover;
+
+  std::map<std::string, cyclus::toolkit::ResBuf<cyclus::Material> > streambufs;
+  };
+
+}  // namespace cycamore
+
+#endif  // CYCAMORE_SRC_SEPARATIONS_H_
