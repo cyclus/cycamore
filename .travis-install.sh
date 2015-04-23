cdf4ab29ed9511ee7db06f9ecf3c7dd56f4fd3c5
Matthew Gidden <matthew.gidden@gmail.com>
2015-04-23 11:38:35 -0500

    explicit status var

--- .travis-install.sh
+++ .travis-install.sh
@@ -3,10 +3,12 @@ unzip -j ciclus.zip "*/cycamore/*" -d conda-recipe
 sed -i  "s/- cyclus/- cyclus 0.0/g" conda-recipe/meta.yaml
 conda build --no-test conda-recipe
 
-if [[ $? != 0 ]]; then
-    exit $?
+status=$?
+if [[ $status != 0 ]]; then
+    exit $status
 
 conda install --use-local cycamore=0.0
 
-if [[ $? != 0 ]]; then
-    exit $?
+status=$?
+if [[ $status != 0 ]]; then
+    exit $status
