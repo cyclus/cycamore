08499af805a4d3edc6b3140869ef3c28f466fc84
Matthew Gidden <matthew.gidden@gmail.com>
2015-04-23 11:19:12 -0500

    early exit on failure

--- .travis-install.sh
+++ .travis-install.sh
@@ -2,4 +2,11 @@ wget https://github.com/gidden/ciclus/archive/travis.zip -O ciclus.zip
 unzip -j ciclus.zip "*/cycamore/*" -d conda-recipe
 sed -i  "s/- cyclus/- cyclus 0.0/g" conda-recipe/meta.yaml
 conda build --no-test conda-recipe
+
+if [[ $? != 0 ]]; then
+    exit $?
+
 conda install --use-local cycamore=0.0
+
+if [[ $? != 0 ]]; then
+    exit $?
