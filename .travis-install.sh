291b625f279b12e0aa9230ee550e7ab4d2cce054
Matthew Gidden <matthew.gidden@gmail.com>
2015-04-28 16:18:35 -0500

    conda->conda-recipe

--- .travis-install.sh
+++ .travis-install.sh
@@ -8,10 +8,10 @@ msg=`git log --pretty=oneline -1`
 echo "Building commit: $msg" 
 
 # setup conda recipe to use develop cyclus
-sed -i  "s/- cyclus/- cyclus 0.0/g" conda/meta.yaml
+sed -i  "s/- cyclus/- cyclus 0.0/g" conda-recipe/meta.yaml
 
 # build
-conda build --no-test conda
+conda build --no-test conda-recipe
 
 # install
 conda install --use-local cycamore=0.0
