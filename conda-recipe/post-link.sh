d4dd024a0f6c0edb2bce8d55571c17b1e4593630
Matthew Gidden <matthew.gidden@gmail.com>
2015-05-01 10:10:19 -0500

    kill extra space

--- conda-recipe/post-link.sh
+++ conda-recipe/post-link.sh
@@ -5,7 +5,7 @@ mv $PREFIX/bin/cycamore_unit_tests $PREFIX/bin/cycamore_unit_tests_base
 echo "
 #!/bin/bash
 export LD_LIBRARY_PATH=$PREFIX/lib:$PREFIX/lib/cyclus
- export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib/cyclus
+export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib/cyclus
 export CYCLUS_NUC_DATA=$PREFIX/share/cyclus/cyclus_nuc_data.h5
 export CYCLUS_PATH=$PREFIX/lib/cyclus
 export CYCLUS_RNG_SCHEMA=$PREFIX/share/cyclus/cyclus.rng.in
