827dc10c244d82f95a380835edf45ee02bfab851
Meghan McGarry <mbmcgarry@wisc.edu>
2015-05-06 16:28:17 -0500

    A few more typos in the EF docstring

--- src/enrichment_facility.h
+++ src/enrichment_facility.h
@@ -116,31 +116,31 @@ class Enrichment : public cyclus::Facility {
 #pragma cyclus note {   	  \
   "niche": "enrichment facility",				  \
   "doc":								\
-  "The Enrichment facilityis a simple Agent that enriches natural"	 \
-  "uranium in a Cyclus simulation. It does not explicitly compute"	\
-  "the physical enrichment process, rather it calculates the SWU"	\
-  "required to convert an source uranium recipe (ie. natural uranium)"	\
-  "into a requested enriched recipe (ie. 4% enriched uranium), given"	\
-  "the natural uranium inventory constraint and its SWU capacity"	\
-  "constraint."								\
+  "The Enrichment facility is a simple agent that enriches natural "	 \
+  "uranium in a Cyclus simulation. It does not explicitly compute "	\
+  "the physical enrichment process, rather it calculates the SWU "	\
+  "required to convert an source uranium recipe (i.e. natural uranium) " \
+  "into a requested enriched recipe (i.e. 4% enriched uranium), given " \
+  "the natural uranium inventory constraint and its SWU capacity " \
+  "constraint."							\
   "\n\n"								\
-  "The Enrichment facility requests an input commodity and associated recipe" \
-  "whose quantity is its remaining inventory capacity.  All facilities" \
-  "trading the same input commodity (even with different recipes) will" \
-  "offer materials for trade.  The Enrichment facility accepts any input" \
-  "materials with enrichments less than its tails assay, as long as some" \
-  "U235 is present, and preference increases with U235 content.  If no" \
-  "U235 is present in the offered material, the trade preference is set" \
-  "to -1 and the material is not accepted.  Any material components other" \
-  "other than U235 and U238 are sent directly to the tails buffer."	\
+  "The Enrichment facility requests an input commodity and associated " \
+  "recipe whose quantity is its remaining inventory capacity.  All " \
+  "facilities trading the same input commodity (even with different " \
+  "recipes) will offer materials for trade.  The Enrichment facility " \
+  "accepts any input materials with enrichments less than its tails assay, "\
+  "as long as some U235 is present, and preference increases with U235 " \
+  "content.  If no U235 is present in the offered material, the trade " \
+  "preference is set to -1 and the material is not accepted.  Any material " \
+  "components other than U235 and U238 are sent directly to the tails buffer."\
   "\n\n"								\
-  "The Enrichment facility will bid on any request for its output commodity" \
-  "up to the maximum allowed enrichment (if not specified, default is 100%)" \
-  "It bids on either the request quantity, or the maximum quanity allowed" \
-  "by its SWU constraint or natural uranium inventory, whichever is lower." \
-  "If multiple output commodities with different enrichment levels are" \
-  "requested and the facility does not have the SWU or quantity capacity" \
-  "to meet all requests, the requests are fully, then partially filled" \
+  "The Enrichment facility will bid on any request for its output commodity "\
+  "up to the maximum allowed enrichment (if not specified, default is 100%) "\
+  "It bids on either the request quantity, or the maximum quanity allowed " \
+  "by its SWU constraint or natural uranium inventory, whichever is lower. " \
+  "If multiple output commodities with different enrichment levels are " \
+  "requested and the facility does not have the SWU or quantity capacity " \
+  "to meet all requests, the requests are fully, then partially filled " \
   "in unspecified but repeatable order."				\
   "\n\n"								\
   "Accumulated tails inventory is offered for trading as a specifiable " \
