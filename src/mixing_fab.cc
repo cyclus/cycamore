0f06f0b0eed5a102a0c1f77a235cae49d245aa81
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-04-14 14:00:55 -0500

    cleaning..

--- src/mixing_fab.cc
+++ src/mixing_fab.cc
@@ -10,177 +10,171 @@ using pyne::simple_xs;
 namespace cycamore {
   
   
-  MixingFab::MixingFab(cyclus::Context* ctx)
-  : cyclus::Facility(ctx), throughput(0) {
-    cyclus::Warn<cyclus::EXPERIMENTAL_WARNING>(
-       "the MixingFab archetype "
-       "is experimental");
-  }
-  
-  //********************************************//
-  cyclus::Inventories MixingFab::SnapshotInv() {
-    cyclus::Inventories invs;
-    
-    // these inventory names are intentionally convoluted so as to not clash
-    // with the user-specified stream commods that are used as the MixingFab
-    // streams inventory names.
-    invs["output-inv-name"] = output.PopNRes(output.count());
-    output.Push(invs["output-inv-name"]);
-    
-    std::map<std::string, ResBuf<Material> >::iterator it;
-    for (it = streambufs.begin(); it != streambufs.end(); ++it) {
-      invs[it->first] = it->second.PopNRes(it->second.count());
-      it->second.Push(invs[it->first]);
-    }
-    return invs;
-  }
-  
-  //********************************************//
-  void MixingFab::InitInv(cyclus::Inventories& inv) {
-    inv["output-inv-name"] = output.PopNRes(output.count());
-    output.Push(inv["output-inv-name"]);
-    
-    cyclus::Inventories::iterator it;
-    for (it = inv.begin(); it != inv.end(); ++it) {
-      streambufs[it->first].Push(it->second);
-    }
-  }
-  
-  
-//********************************************//
-  void MixingFab::EnterNotify() {
-    cyclus::Facility::EnterNotify();
+MixingFab::MixingFab(cyclus::Context* ctx)
+: cyclus::Facility(ctx), throughput(0) {
+  cyclus::Warn<cyclus::EXPERIMENTAL_WARNING>(
+     "the MixingFab archetype "
+     "is experimental");
+}
 
-    if (mixing_frac.empty()) {
-      for (int i = 0; i < in_commods.size(); i++) {
-        mixing_frac.push_back(1/in_commods.size());
-      }
+cyclus::Inventories MixingFab::SnapshotInv() {
+  cyclus::Inventories invs;
+  
+  // these inventory names are intentionally convoluted so as to not clash
+  // with the user-specified stream commods that are used as the MixingFab
+  // streams inventory names.
+  invs["output-inv-name"] = output.PopNRes(output.count());
+  output.Push(invs["output-inv-name"]);
+  
+  std::map<std::string, ResBuf<Material> >::iterator it;
+  for (it = streambufs.begin(); it != streambufs.end(); ++it) {
+    invs[it->first] = it->second.PopNRes(it->second.count());
+    it->second.Push(invs[it->first]);
+  }
+  return invs;
+}
+
+void MixingFab::InitInv(cyclus::Inventories& inv) {
+  inv["output-inv-name"] = output.PopNRes(output.count());
+  output.Push(inv["output-inv-name"]);
+  
+  cyclus::Inventories::iterator it;
+  for (it = inv.begin(); it != inv.end(); ++it) {
+    streambufs[it->first].Push(it->second);
+  }
+}
+
+
+void MixingFab::EnterNotify() {
+  cyclus::Facility::EnterNotify();
+
+  if (mixing_frac.empty()) {
+    for (int i = 0; i < in_commods.size(); i++) {
+      mixing_frac.push_back(1/in_commods.size());
+    }
+  
+  } else if (mixing_frac.size() != in_commods.size()) {
+    std::stringstream ss;
+    ss << "prototype '" << prototype() << "' has " << mixing_frac.size()
+    << " commodity frqction values, expected " << in_commods.size();
+    throw cyclus::ValidationError(ss.str());
+  
+  } else {
+    double frac_sum = 0;
+    for( int i = 0; i < mixing_frac.size(); i++)
+      frac_sum += mixing_frac[i];
     
-    } else if (mixing_frac.size() != in_commods.size()) {
+    if(frac_sum != 1.) {
       std::stringstream ss;
       ss << "prototype '" << prototype() << "' has " << mixing_frac.size()
       << " commodity frqction values, expected " << in_commods.size();
-      throw cyclus::ValidationError(ss.str());
-    
-    } else {
-      double frac_sum = 0;
-      for( int i = 0; i < mixing_frac.size(); i++)
-        frac_sum += mixing_frac[i];
-      
-      if(frac_sum != 1.) {
-        std::stringstream ss;
-        ss << "prototype '" << prototype() << "' has " << mixing_frac.size()
-        << " commodity frqction values, expected " << in_commods.size();
-        cyclus::Warn<cyclus::VALUE_WARNING>(ss.str());
-      }
-      
-      for( int i = 0; i < mixing_frac.size(); i++){
-        mixing_frac[i] *= 1./frac_sum;
-      }
-      
+      cyclus::Warn<cyclus::VALUE_WARNING>(ss.str());
     }
     
-    for( int i = 0; i < in_commods.size(); i++){
-      std::string name = in_commods[i];
-      double cap = in_buf_size[i];
-      if (cap >= 0) {
-        streambufs[name].capacity(cap);
-      }
+    for( int i = 0; i < mixing_frac.size(); i++){
+      mixing_frac[i] *= 1./frac_sum;
     }
     
-    
-    sell_policy.Init(this, &output, "output").Set(out_commod).Start();
-
   }
   
-  //********************************************//
-  void MixingFab::Tick(){
+  for( int i = 0; i < in_commods.size(); i++){
+    std::string name = in_commods[i];
+    double cap = in_buf_size[i];
+    if (cap >= 0) {
+      streambufs[name].capacity(cap);
+    }
+  }
+  
+  
+  sell_policy.Init(this, &output, "output").Set(out_commod).Start();
+
+}
+
+void MixingFab::Tick(){
+  
+  if(output.quantity() < output.capacity()){
+
+    double tgt_qty = output.space();
+
+    for( int i = 0; i < in_commods.size(); i++){
+      std::string name = in_commods[i];
+      tgt_qty = std::min(tgt_qty, streambufs[name].quantity()/ mixing_frac[i] );
+    }
     
-    if(output.quantity() < output.capacity()){
+    tgt_qty = std::min(tgt_qty, throughput);
 
-      double tgt_qty = output.space();
-
+    if(tgt_qty > 0){
+      Material::Ptr m;
       for( int i = 0; i < in_commods.size(); i++){
         std::string name = in_commods[i];
-        tgt_qty = std::min(tgt_qty, streambufs[name].quantity()/ mixing_frac[i] );
-      }
-      
-      tgt_qty = std::min(tgt_qty, throughput);
-
-      if(tgt_qty > 0){
-        Material::Ptr m;
-        for( int i = 0; i < in_commods.size(); i++){
-          std::string name = in_commods[i];
-          if(i==0){
-            m = streambufs[name].Pop(mixing_frac[i] *tgt_qty);
-          } else {
-            Material::Ptr m_ = streambufs[name].Pop(mixing_frac[i] *tgt_qty);
-            m->Absorb(m_);
-          }
+        if(i==0){
+          m = streambufs[name].Pop(mixing_frac[i] *tgt_qty);
+        } else {
+          Material::Ptr m_ = streambufs[name].Pop(mixing_frac[i] *tgt_qty);
+          m->Absorb(m_);
         }
-
-        output.Push(m);
       }
-      
+
+      output.Push(m);
     }
     
   }
   
-//********************************************//
-  std::set<cyclus::RequestPortfolio<Material>::Ptr> MixingFab::GetMatlRequests() {
-    using cyclus::RequestPortfolio;
+}
 
-    std::set<RequestPortfolio<Material>::Ptr> ports;
-    
-    for( int i = 0; i < in_commods.size(); i++){
-      std::string name = in_commods[i];
+std::set<cyclus::RequestPortfolio<Material>::Ptr> MixingFab::GetMatlRequests() {
+  using cyclus::RequestPortfolio;
 
-      if (streambufs[name].space() > cyclus::eps()) {
-        RequestPortfolio<Material>::Ptr port(new RequestPortfolio<Material>());
-        
-        Material::Ptr m = cyclus::NewBlankMaterial(streambufs[name].space());
-        
-        cyclus::Request<Material>* r;
-        r = port->AddRequest(m, this, in_commods[i], 1., false);
-        req_inventories_[r] = name;
-        ports.insert(port);
-      }
+  std::set<RequestPortfolio<Material>::Ptr> ports;
+  
+  for( int i = 0; i < in_commods.size(); i++){
+    std::string name = in_commods[i];
+
+    if (streambufs[name].space() > cyclus::eps()) {
+      RequestPortfolio<Material>::Ptr port(new RequestPortfolio<Material>());
       
+      Material::Ptr m = cyclus::NewBlankMaterial(streambufs[name].space());
+      
+      cyclus::Request<Material>* r;
+      r = port->AddRequest(m, this, in_commods[i], 1., false);
+      req_inventories_[r] = name;
+      ports.insert(port);
     }
-    return ports;
+    
+  }
+  return ports;
+}
+
+void MixingFab::AcceptMatlTrades(const std::vector<
+  std::pair<cyclus::Trade<Material>, Material::Ptr> >& responses) {
+  
+  std::vector<std::pair<cyclus::Trade<cyclus::Material>,
+  cyclus::Material::Ptr> >::const_iterator trade;
+
+  for (trade = responses.begin(); trade != responses.end(); ++trade) {
+    
+    cyclus::Request<Material>* req = trade->first.request;
+    Material::Ptr m = trade->second;
+    
+    std::string name = req_inventories_[req];
+    bool assigned = false;
+    std::map<std::string, cyclus::toolkit::ResBuf<cyclus::Material> >::iterator it;
+
+    for( it = streambufs.begin(); it != streambufs.end(); it++ ){
+      if( name == it->first){
+        it->second.Push(m);
+        assigned = true;
+        break;
+      }
+    }
+    if( !assigned ){
+      throw cyclus::ValueError("cycamore::MixingFab was overmatched on requests");
+    }
   }
   
-//********************************************//
-  void MixingFab::AcceptMatlTrades(const std::vector<
-    std::pair<cyclus::Trade<Material>, Material::Ptr> >& responses) {
-    
-    std::vector<std::pair<cyclus::Trade<cyclus::Material>,
-    cyclus::Material::Ptr> >::const_iterator trade;
-
-    for (trade = responses.begin(); trade != responses.end(); ++trade) {
-      
-      cyclus::Request<Material>* req = trade->first.request;
-      Material::Ptr m = trade->second;
-      
-      std::string name = req_inventories_[req];
-      bool assigned = false;
-      std::map<std::string, cyclus::toolkit::ResBuf<cyclus::Material> >::iterator it;
-
-      for( it = streambufs.begin(); it != streambufs.end(); it++ ){
-        if( name == it->first){
-          it->second.Push(m);
-          assigned = true;
-          break;
-        }
-      }
-      if( !assigned ){
-        throw cyclus::ValueError("cycamore::MixingFab was overmatched on requests");
-      }
-    }
-    
-    req_inventories_.clear();
-    
-  }
+  req_inventories_.clear();
+  
+}
   
 
 extern "C" cyclus::Agent* ConstructMixingFab(cyclus::Context* ctx) {
