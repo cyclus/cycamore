82a134cc67077502e34f63c2706367153926d3f1
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-04-18 14:28:36 -0500

    add check of the quantity exhanged in CompleteMixingProcess test

--- src/mixing_fab_tests.cc
+++ src/mixing_fab_tests.cc
@@ -342,6 +342,11 @@ namespace cycamore {
     conds.push_back(Cond("Commodity", "==", std::string("mixedstream")));
     QueryResult qr = sim.db().Query("Transactions", &conds);
     EXPECT_EQ(1, qr.rows.size());
+    
+    
+    Material::Ptr m = sim.GetMaterial(qr.GetVal<int>("ResourceId"));
+    EXPECT_DOUBLE_EQ(1., m->quantity());
+
   }
 
   
