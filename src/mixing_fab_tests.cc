74e18b7644cf01e878c5b661d47ffcf4b7fea8ec
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-04-18 12:28:34 -0500

    add test to check buying material, mixing material then selling material properly

--- src/mixing_fab_tests.cc
+++ src/mixing_fab_tests.cc
@@ -178,8 +178,93 @@ namespace cycamore {
     
   }
 
+  // Check the correct mixing composition
+  TEST_F(MixingFabTest, MixingComposition) {
+    
+    std::vector<double> in_frac_ = { 0.80, 0.15, 0.05 };
+    
+    SetStream_ratio(in_frac_);
+    
+    SetOutStream_capacity(50);
+    
+    SetThroughput(1e200);
+    
+    std::vector<Material::Ptr> mat;
+    mat.push_back(Material::CreateUntracked(in_cap[0], c_natu()));
+    mat.push_back(Material::CreateUntracked(in_cap[1], c_pustream()));
+    mat.push_back(Material::CreateUntracked(in_cap[2], c_uox()));
+    
+    SetInputInv(mat);
+    mf_facility_->Tick();
+    
+    CompMap v_0 = c_natu()->mass();
+    Normalize(&v_0, in_frac[0]);
+    CompMap v_1 = c_pustream()->mass();
+    Normalize(&v_1, in_frac[1]);
+    CompMap v_2 = c_uox()->mass();
+    Normalize(&v_2, in_frac[2]);
+    CompMap v = v_0;
+    v = Add( v, v_1);
+    v = Add( v, v_2);
+    
+    InvBuffer* buffer = GetOutPutBuffer();
+    Material::Ptr final_mat = cyclus::ResCast<Material>(buffer->PopBack());
+    cyclus::CompMap final_comp = final_mat->comp()->mass();
+    
+    Normalize(&v,1);
+    Normalize(&final_comp,1);
+    
+    cyclus::CompMap sum_v = Add(v, final_comp);
+    
+    CompMap::iterator it;
+    for(it = sum_v.begin(); it != sum_v.end(); it ++){
+      EXPECT_DOUBLE_EQ(final_comp[it->first],v[it->first]) << "Unexpected difference on nuclide " << it->first << ".";
+    }
+  }
   
-  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  
+  
+  // Check the throughput constrain
+  TEST_F(MixingFabTest, Throughput) {
+    
+    std::vector<double> in_frac_ = { 0.80, 0.15, 0.05 };
+    
+    SetStream_ratio(in_frac_);
+    
+    SetOutStream_capacity(50);
+    
+    SetThroughput(0.5);
+    
+    std::vector<Material::Ptr> mat;
+    mat.push_back(Material::CreateUntracked(in_cap[0], c_natu()));
+    mat.push_back(Material::CreateUntracked(in_cap[1], c_pustream()));
+    mat.push_back(Material::CreateUntracked(in_cap[2], c_uox()));
+    SetInputInv(mat);
+    
+    mf_facility_->Tick();
+    
+    std::vector<double> cap;
+    for(int i = 0; i < in_com.size(); i++ ){
+      cap.push_back(in_cap[i]-0.5*in_frac[i]);
+    }
+    
+    std::map<std::string, InvBuffer> streambuf = GetStreamBuffer();
+    
+    for (int i = 0; i < in_com.size(); i++) {
+      std::string buf_com = in_com[i];
+      double buf_size = in_cap[i];
+      double buf_ratio = in_frac[i];
+      double buf_inv = streambuf[buf_com].quantity();
+      
+      // checking that each input buf was reduce of the correct amount (constrained by the throughput"
+      EXPECT_EQ( buf_size - 0.5*buf_ratio, buf_inv) << " one (or more) input buffer inventory was not drawn from in the correct ratio.";
+    }
+    
+    
+    // output buffer size should be equal to the throuput size
+    EXPECT_EQ(throughput, GetOutPutBuffer()->quantity()) << " mixing was not correctly constrained by throughput.";
+    
+  }
 
   // multiple input streams can be correctly requested and used as
   //  material inventory.
@@ -224,90 +309,41 @@ namespace cycamore {
     qr = sim.db().Query("Transactions", &conds);
     EXPECT_EQ(1, qr.rows.size());
   }
+
   
   
-  TEST_F(MixingFabTest, MixingComposition) {
+  // multiple input streams can be correctly requested and used as
+  //  material inventory.
+  TEST(MixingFabTests, CompleteMixingProcess) {
+    std::string config =
+    "<in_commods> <val>stream1</val> <val>stream2</val> <val>stream3</val> </in_commods>"
+    "<in_buf_size> <val>2.5</val> <val>3</val> <val>5</val></in_buf_size>"
+    "<mixing_ratio> <val>0.8</val> <val>0.15</val> <val>0.05</val></mixing_ratio>"
+    ""
+    "<out_commod>mixedstream</out_commod>"
+    "<outputbuf_size>10</outputbuf_size>"
+    "<throughput>1</throughput>"
+    ;
     
-    std::vector<double> in_frac_ = { 0.80, 0.15, 0.05 };
+    int simdur = 2;
+    cyclus::MockSim sim(cyclus::AgentSpec(":cycamore:MixingFab"), config, simdur);
+    sim.AddSource("stream1").recipe("unatstream").capacity(1).Finalize();
+    sim.AddSource("stream2").recipe("uoxstream").capacity(1).Finalize();
+    sim.AddSource("stream3").recipe("pustream").capacity(1).Finalize();
+    sim.AddRecipe("unatstream", c_natu());
+    sim.AddRecipe("uoxstream", c_pustream());
+    sim.AddRecipe("pustream", c_uox());
     
-    SetStream_ratio(in_frac_);
-    
-    SetOutStream_capacity(50);
-    
-    SetThroughput(1e200);
-    
-    std::vector<Material::Ptr> mat;
-    mat.push_back(Material::CreateUntracked(in_cap[0], c_natu()));
-    mat.push_back(Material::CreateUntracked(in_cap[1], c_pustream()));
-    mat.push_back(Material::CreateUntracked(in_cap[2], c_uox()));
-    
-    SetInputInv(mat);
-    mf_facility_->Tick();
-    
-    CompMap v_0 = c_natu()->mass();
-    Normalize(&v_0, in_frac[0]);
-    CompMap v_1 = c_pustream()->mass();
-    Normalize(&v_1, in_frac[1]);
-    CompMap v_2 = c_uox()->mass();
-    Normalize(&v_2, in_frac[2]);
-    CompMap v = v_0;
-    v = Add( v, v_1);
-    v = Add( v, v_2);
-    
-    InvBuffer* buffer = GetOutPutBuffer();
-    Material::Ptr final_mat = cyclus::ResCast<Material>(buffer->PopBack());
-    cyclus::CompMap final_comp = final_mat->comp()->mass();
-    
-    Normalize(&v,1);
-    Normalize(&final_comp,1);
-    
-    cyclus::CompMap sum_v = Add(v, final_comp);
-    
-    CompMap::iterator it;
-    for(it = sum_v.begin(); it != sum_v.end(); it ++){
-      EXPECT_DOUBLE_EQ(final_comp[it->first],v[it->first]) << "Unexpected difference on nuclide " << it->first << ".";
-    }
-  }
+    sim.AddSink("mixedstream").capacity(10).Finalize();
+    int id = sim.Run();
 
-  TEST_F(MixingFabTest, Throughput) {
-    
-    std::vector<double> in_frac_ = { 0.80, 0.15, 0.05 };
-    
-    SetStream_ratio(in_frac_);
-    
-    SetOutStream_capacity(50);
-    
-    SetThroughput(0.5);
-    
-    std::vector<Material::Ptr> mat;
-    mat.push_back(Material::CreateUntracked(in_cap[0], c_natu()));
-    mat.push_back(Material::CreateUntracked(in_cap[1], c_pustream()));
-    mat.push_back(Material::CreateUntracked(in_cap[2], c_uox()));
-    SetInputInv(mat);
-    
-    mf_facility_->Tick();
-    
-    std::vector<double> cap;
-    for(int i = 0; i < in_com.size(); i++ ){
-      cap.push_back(in_cap[i]-0.5*in_frac[i]);
+    // Checking that all input stream get one transaction each.
+    std::vector<Cond> conds;
+    conds.push_back(Cond("Commodity", "==", std::string("mixedstream")));
+    QueryResult qr = sim.db().Query("Transactions", &conds);
+    EXPECT_EQ(1, qr.rows.size());
     }
-    
-    std::map<std::string, InvBuffer> streambuf = GetStreamBuffer();
-    
-    for (int i = 0; i < in_com.size(); i++) {
-      std::string buf_com = in_com[i];
-      double buf_size = in_cap[i];
-      double buf_ratio = in_frac[i];
-      double buf_inv = streambuf[buf_com].quantity();
-      
-      // checking that each input buf was reduce of the correct amount (constrained by the throughput"
-      EXPECT_EQ( buf_size - 0.5*buf_ratio, buf_inv) << " one (or more) input buffer inventory was not drawn from in the correct ratio.";
-    }
-    
-    
-    // output buffer size should be equal to the throuput size
-    EXPECT_EQ(throughput, GetOutPutBuffer()->quantity()) << " mixing was not correctly constrained by throughput.";
-    
-  }
 
+  
+  
 } // namespace cycamore
