8dc4f8d05d0dc91f7cb6f07e84709da9a275f468
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-08-01 11:40:50 -0500

    change XX and HASH template variables to CYCAMORE_UBUNTU_MAJOR_VERSION, CYCAMORE_REPO_HASH, add a tmp Dockerfile for the second sed to work...

--- docker/release-ci/build_udpload_deb.sh
+++ docker/release-ci/build_udpload_deb.sh
@@ -4,8 +4,10 @@ if [ $# -ne 1 ]; then
   echo "illegal number of parameters"
 fi
 
-sed -e s/XX/${1}/g docker/release-ci/Dockerfile_sample > Dockerfile
-sed -e s/HASH/`git rev-parse --short HEAD`/g docker/release-ci/Dockerfile > Dockerfile
+sed -e s/CYCAMORE_UBUNTU_MAJOR_VERSION/${1}/g docker/release-ci/Dockerfile_sample > Dockerfile_
+sed -e s/CYCAMORE_REPO_HASH/`git rev-parse --short HEAD`/g Dockerfile_ > Dockerfile
+rm Dockerfile_
+
 
 docker build -t deb/ubuntu-${1} . # build cyclus against ubuntu 14.04 in the docker container
 docker create --name=deb-${1}.04 deb/ubuntu-${1} # build deb in docker
