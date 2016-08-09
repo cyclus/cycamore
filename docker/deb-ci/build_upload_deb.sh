7861cac5e731188070d47791c9b89be429edcfd7
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-08-09 09:17:58 -0500

    modify build script accordingly to cyclus one

--- docker/deb-ci/build_upload_deb.sh
+++ docker/deb-ci/build_upload_deb.sh
@@ -2,12 +2,12 @@
 
 if [ $# -ne 1 ]; then 
   echo "illegal number of parameters"
+  exit 1
 fi
 
-sed -e s/CYCAMORE_UBUNTU_MAJOR_VERSION/${1}/g docker/release-ci/Dockerfile_template > Dockerfile_
-sed -e s/CYCAMORE_REPO_HASH/`git rev-parse --short HEAD`/g Dockerfile_ > Dockerfile
-rm Dockerfile_
-
+rm Dockerfile
+sed -e s/CYCAMORE_UBUNTU_MAJOR_VERSION/${1}/g docker/release-ci/Dockerfile_template > Dockerfile
+sed -i s/CYCAMORE_REPO_HASH/`git rev-parse --short HEAD`/g Dockerfile
 
 docker build -t deb/ubuntu-${1} . # build cyclus against ubuntu 14.04 in the docker container
 docker create --name=deb-${1}.04 deb/ubuntu-${1} # build deb in docker
