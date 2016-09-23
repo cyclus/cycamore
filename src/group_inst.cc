08f4047552542ebe747def0ab848f8d6b1eefa3f
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-09-23 10:16:39 -0500

    working group_inst

---
+++ src/group_inst.cc
@@ -0,0 +1,31 @@
+// Implements the GroupInst class
+#include "group_inst.h"
+
+namespace cycamore {
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+GroupInst::GroupInst(cyclus::Context* ctx) : cyclus::Institution(ctx) { }
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+GroupInst::~GroupInst() {}
+
+
+void GroupInst::EnterNotify() {
+  cyclus::Institution::EnterNotify();
+
+  for (int i = 0; i < prototypes.size(); i++) {
+    std::string s_proto = prototypes[i];
+    context()->SchedBuild(this, s_proto);  //builds on next timestep
+    BuildNotify(this);
+  }
+  std::cout << "GROUP DEPLOY!!!! at " << context()->time() << std::endl;
+}
+
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+extern "C" cyclus::Agent* ConstructGroupInst(cyclus::Context* ctx) {
+  return new GroupInst(ctx);
+}
+
+}  // namespace cycamore
