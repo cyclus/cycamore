f7da6a381acc60333da967b8a2090147be1f902c
bennibbelink <79653949+bennibbelink@users.noreply.github.com>
2024-07-21 16:08:24 -0400

    replace CY_LARGE_INT in doc

--- src/sink.h
+++ src/sink.h
@@ -254,7 +254,7 @@ class Sink
                       "uitype": "range", \
                       "range": [1, CY_LARGE_INT], \
                       "doc": "When a random distribution is used to determine the " \
-                             "frequency of the request, this is the upper bound. Default CY_LARGE_INT (1e299)"}
+                             f"frequency of the request, this is the upper bound. Default {CY_LARGE_INT} (CY_LARGE_INT)"}
   int random_frequency_max;
 
   #pragma cyclus var { \
